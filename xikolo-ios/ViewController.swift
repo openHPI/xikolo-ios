f7280824eff33ab30797b9624233b9d3189f2538
Jan Renz <jan.renz@gmail.com>
2015-06-25 11:06:29 +0200

    Initial Commit

---
+++ xikolo-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  xikolo-ios
+//
+//  Created by Jan Renz on 25/06/15.
+//  Copyright © 2015 HPI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ xikolo-iosUITests/xikolo_iosUITests.swift
@@ -0,0 +1,35 @@
+//
+//  xikolo_iosUITests.swift
+//  xikolo-iosUITests
+//
+//  Created by Jan Renz on 25/06/15.
+//  Copyright © 2015 HPI. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class xikolo_iosUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}

46c4b43f2ddd4574d612f470c94bab5486ca8c60
Arne Boockmeyer <mail@arneboockmeyer.de>
2015-06-25 17:53:06 +0200

    Add basic model for course (list)

---
+++ xikolo-ios/CourseList.swift
@@ -0,0 +1,15 @@
+//
+//  CourseList.swift
+//  xikolo-ios
+//
+//  Created by Arne Boockmeyer on 25/06/15.
+//  Copyright © 2015 HPI. All rights reserved.
+//
+
+import Foundation
+
+class CourseList : NSObject {
+
+    var courseList : NSArray = []
+
+}

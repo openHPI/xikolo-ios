ed86839356d7faecf96d582e46b5b75dcc8225d8
Arne Boockmeyer <mail@arneboockmeyer.de>
2015-07-08 13:05:33 +0200

    Add own cell for the courses

---
+++ xikolo-ios/CoursesOverviewViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CoursesOverviewViewCell.swift
+//  xikolo-ios
+//
+//  Created by Arne Boockmeyer on 08/07/15.
+//  Copyright © 2015 HPI. All rights reserved.
+//
+
+import UIKit
+
+class CoursesOverviewViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var coursesImageView: UIImageView!
+    @IBOutlet weak var coursesTitleLabel: UILabel!
+    
+}

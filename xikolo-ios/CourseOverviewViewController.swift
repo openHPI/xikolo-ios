883c3c7a233fbeb2371026de4890da35bc79a8ea
Arne Boockmeyer <mail@arneboockmeyer.de>
2015-07-08 12:32:33 +0200

    Add collection view to show the courses and his view controller

---
+++ xikolo-ios/CourseOverviewViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CourseOverviewViewController.swift
+//  xikolo-ios
+//
+//  Created by Arne Boockmeyer on 08/07/15.
+//  Copyright © 2015 HPI. All rights reserved.
+//
+
+import UIKit
+
+class CourseOverviewViewController: UICollectionViewController {
+
+}

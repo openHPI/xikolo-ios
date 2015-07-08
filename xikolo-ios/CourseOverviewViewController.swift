6d7aa461750a72f0f9bd39d1499c8d02440574f1
Arne Boockmeyer <mail@arneboockmeyer.de>
2015-07-08 12:56:23 +0200

    Add basic logic for the view controller

--- xikolo-ios/CourseOverviewViewController.swift
+++ xikolo-ios/CourseOverviewViewController.swift
@@ -10,4 +10,42 @@ import UIKit
 
 class CourseOverviewViewController: UICollectionViewController {
 
+    private var courses : CourseList = DataManager.getAllCourses()
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let reuseIdentifier = "CourseCell"
+
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+}
+extension CourseOverviewViewController {
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return courses.courseList.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return courses.courseList.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        cell.backgroundColor = UIColor.blackColor()
+        // Configure the cell
+        return cell
+    }
+}
+extension CourseOverviewViewController : UICollectionViewDelegateFlowLayout {
+    func collectionView(collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+            return CGSize(width: 100, height: 100)
+    }
+    
+    func collectionView(collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+            return sectionInsets
+    }
 }

a3f1b2d223ab8d019833f2e5c3fd0e9a14c48f16
Jonas Müller <mueller.jonas95@gmail.com>
2015-07-16 17:58:43 +0200

    Added alternative course layout

---
+++ xikolo-ios/CourseCell.swift
@@ -0,0 +1,20 @@
+//
+//  CourseCell.swift
+//  xikolo-ios
+//
+//  Created by Jonas Müller on 16.07.15.
+//  Copyright © 2015 HPI. All rights reserved.
+//
+
+import UIKit
+
+class CourseCell: UICollectionViewCell {
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    
+    @IBOutlet weak var buttonEnter: UIButton!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var teacherLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}

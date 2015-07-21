aec8c7d80b97541d32c39d30439cd3dc0ee5fcb9
Jonas Müller <mueller.jonas95@gmail.com>
2015-07-21 15:58:00 +0200

    Fixed CollectionView not displaying Views

--- xikolo-ios/CourseOverviewViewController.swift
+++ xikolo-ios/CourseOverviewViewController.swift
@@ -13,26 +13,40 @@ class CourseOverviewViewController: UICollectionViewController {
     private var courses : CourseList = DataManager.getAllCourses()
     private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
     private let reuseIdentifier = "CourseCell"
+    
+    private var width : CGFloat?
 
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+//        self.collectionView!.registerClass(CourseCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        self.collectionView!.registerNib(UINib(nibName: reuseIdentifier, bundle: nil), forCellWithReuseIdentifier: reuseIdentifier)
+        
+        width = self.collectionView!.frame.size.width - 20
     }
     
 }
 extension CourseOverviewViewController {
     
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return courses.courseList.count
+        return 1
     }
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return courses.courseList.count
+        return 3
     }
     
-    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as UICollectionViewCell
-        cell.backgroundColor = UIColor.blackColor()
-        // Configure the cell
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> CourseCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! CourseCell
+        
+        cell.nameLabel.text = "Name"
+        cell.teacherLabel.text = "Teacher"
+        cell.dateLabel.text = "Date"
+        
+        cell.layer.cornerRadius = 3
+        
         return cell
     }
 }
@@ -40,7 +54,7 @@ extension CourseOverviewViewController : UICollectionViewDelegateFlowLayout {
     func collectionView(collectionView: UICollectionView,
         layout collectionViewLayout: UICollectionViewLayout,
         sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-            return CGSize(width: 100, height: 100)
+            return CGSize(width: width!, height: width! * 0.6)
     }
     
     func collectionView(collectionView: UICollectionView,

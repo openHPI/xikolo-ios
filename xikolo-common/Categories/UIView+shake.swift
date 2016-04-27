fcd818f029c135e89a34ddd5c8d09260607c49e3
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2016-04-27 18:42:04 +0200

    Improved shake on tvOS

--- xikolo-common/Categories/UIView+shake.swift
+++ xikolo-common/Categories/UIView+shake.swift
@@ -11,12 +11,17 @@ import UIKit
 extension UIView {
 
     func shake() {
+#if os(iOS)
+        let deltaX = 2.0 as CGFloat
+#else
+        let deltaX = 10.0 as CGFloat
+#endif
         let animation = CABasicAnimation(keyPath: "position")
         animation.duration = 0.1
         animation.repeatCount = 5
         animation.autoreverses = true
-        animation.fromValue = NSValue(CGPoint: CGPointMake(center.x - 2.0, center.y))
-        animation.toValue = NSValue(CGPoint: CGPointMake(center.x + 2.0, center.y))
+        animation.fromValue = NSValue(CGPoint: CGPointMake(center.x - deltaX, center.y))
+        animation.toValue = NSValue(CGPoint: CGPointMake(center.x + deltaX, center.y))
         layer.addAnimation(animation, forKey: "position")
     }
 

95e07a69e96cd21bc575e4159f5520fb32461be4
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2016-04-27 19:03:45 +0200

    Added use of the new abstract login controller to iOS as well

--- xikolo-ios/RegisterViewController.swift
+++ xikolo-ios/RegisterViewController.swift
@@ -8,61 +8,28 @@
 
 import UIKit
 
-class RegisterViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var emailTextField: UITextField! { didSet { emailTextField.delegate = self } }
-    @IBOutlet weak var passwordTextField: UITextField! { didSet { passwordTextField.delegate = self } }
+class RegisterViewController : AbstractLoginViewController, UITextFieldDelegate {
+
     @IBOutlet weak var registerButton: UIButton!
     @IBAction func dismissAction(sender: UIBarButtonItem) {
         presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
-    }
-    
-    @IBAction func loginButton(sender: AnyObject) {
-        let email = emailTextField.text!
-        let password = passwordTextField.text!
-        
-        UserProfileHelper.login(email, password: password, completionHandler: {(token: String?, error: NSError?) -> () in
-            if token != nil {
-                self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
-            } else {
-                self.passwordTextField.shake()
-                // TODO: maybe check whether email is valid
-            }
-        });
     }
 
     @IBAction func registerButton(sender: AnyObject) {
         let url = NSURL(string: Routes.REGISTER_URL)
         UIApplication.sharedApplication().openURL(url!)
-
     }
-    
+
     var pageIndex: Int?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.emailTextField.placeholder = NSLocalizedString("email", comment: "Email")
-        self.passwordTextField.placeholder = NSLocalizedString("password", comment: "Password")
-        self.registerButton.setTitle(NSLocalizedString("register", comment: "Register"), forState: UIControlState.Normal)
-        emailTextField.becomeFirstResponder()
-        
-        // Do any additional setup after loading the view.
+        emailField.becomeFirstResponder()
     }
-    
+
     func textFieldShouldReturn(textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
+
 }

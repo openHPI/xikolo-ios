877b53db9477216745cd54777ba3a549a9201a42
Bjarne Sievers <jellyfishcoding@live.de>
2016-05-26 18:23:21 +0200

    Prepared Views to support editing of profile, moved ProfileTab to a new storyboard

---
+++ xikolo-ios/EditProfileSettingsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  EditProfileSettingsViewController.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 20.05.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileSettingsViewController: UIViewController, UITextFieldDelegate {
+    
+    var currentText = "currentSetting"
+    
+
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textField.becomeFirstResponder()
+        textField.text = currentText
+    }
+    
+    @IBAction func saveAction(sender: UIBarButtonItem) {
+        dismiss(true)
+    }
+    @IBAction func cancelAction(sender: UIBarButtonItem) {
+        dismiss(false)
+    }
+    
+    func dismiss(shouldSave: Bool) -> () {
+        if shouldSave {
+            // TODO: set new Value online
+        }
+        presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
---
+++ xikolo-ios/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 18.05.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+    
+    // TODO: perform and configure Segue to edit dialog
+}

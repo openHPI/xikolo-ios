8214ac4b9be2b600c328da520eebcad0d1155db8
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2016-06-23 17:23:07 +0200

    Added new root view controller with logo

---
+++ xikolo-tvos/ViewControllers/MainViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainViewController.swift
+//  xikolo-ios
+//
+//  Created by Sebastian Brückner on 23.06.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController : UIViewController {
+
+}

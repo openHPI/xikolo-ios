eec9c8910e11dd68ba685edae13cc73286bb55f5
Bjarne Sievers <jellyfishcoding@live.de>
2016-07-06 18:02:19 +0200

    Added news article model

---
+++ xikolo-common/Data/Model/NewsArticle.swift
@@ -0,0 +1,15 @@
+//
+//  NewsArticle.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 04.07.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import CoreData
+import Foundation
+import Spine
+
+class NewsArticle : BaseModel {
+
+}

c651df6a6e637489de1b51b59f185aa3b7faaa23
Bjarne Sievers <jellyfishcoding@live.de>
2016-07-07 11:13:22 +0200

    Added Spine model

--- xikolo-common/Data/Model/NewsArticle.swift
+++ xikolo-common/Data/Model/NewsArticle.swift
@@ -13,3 +13,29 @@ import Spine
 class NewsArticle : BaseModel {
 
 }
+
+class NewsArticleSpine : BaseModelSpine {
+
+    var title: String?
+    var text: String?
+    var published_at: NSDate?
+    var visited: NSNumber?
+
+    override class var cdType: BaseModel.Type {
+        return NewsArticle.self
+    }
+
+    override class var resourceType: ResourceType {
+        return "news-articles"
+    }
+
+    override class var fields: [Field] {
+        return fieldsFromDictionary([
+            "title": Attribute(),
+            "text": Attribute(),
+            "published_at": DateAttribute(),
+            "visited": Attribute(),
+            ])
+    }
+    
+}

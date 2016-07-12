d39756612dc2fb21be0d19e058e44b128f37d665
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2016-07-12 21:34:38 +0200

    Cleaned up ProfileViewController

---
+++ xikolo-common/Categories/CALayer+UIColor.swift
@@ -0,0 +1,22 @@
+//
+//  CALayer+UIColor.swift
+//  xikolo-ios
+//
+//  Created by Sebastian Brückner on 12.07.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import UIKit
+
+extension CALayer {
+
+    @IBInspectable var borderUIColor: UIColor? {
+        get {
+            return borderColor == nil ? nil : UIColor(CGColor: borderColor!)
+        }
+        set(newColor) {
+            borderColor = newColor?.CGColor
+        }
+    }
+
+}
--- xikolo-ios/ViewControllers/Profile/ProfileViewController.swift
+++ xikolo-ios/ViewControllers/Profile/ProfileViewController.swift
@@ -12,58 +12,32 @@ class ProfileViewController: AbstractTabContentViewController {
 
     @IBOutlet weak var headerImage: UIImageView!
     @IBOutlet weak var profileImage: UIImageView!
-    
+
     @IBOutlet weak var container: UIView!
     @IBOutlet weak var logoutButton: UIButton!
 
-    deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
-    }
-    
     @IBAction func logout(sender: UIButton) {
         UserProfileHelper.logout()
-        setViewData()
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ProfileViewController.updateUIAfterLoginLogoutAction), name: NotificationKeys.loginSuccessfulKey, object: nil)
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ProfileViewController.updateUIAfterLoginLogoutAction), name: NotificationKeys.logoutSuccessfulKey, object: nil)
-        setupViews();
-        setViewData();
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        updateUIAfterLoginLogoutAction()
-    }
-    
-    func setupViews() {
-        self.profileImage.layer.cornerRadius = self.profileImage.frame.size.width / 2;
-        self.profileImage.clipsToBounds = true;
-        self.profileImage.layer.borderWidth = 3.0;
-        self.profileImage.layer.borderColor = UIColor.whiteColor().CGColor;
-    }
-    
-    func setViewData() {
-        
-        UserProfileHelper.self.getUser() { (user: UserProfile?, error: NSError?) -> () in
-            if let user = user {
-                // TODO: get name, username etc.
-
-                ImageHelper.loadImageFromURL(user.visual, toImageView: self.profileImage)
-            }
-        };
     }
 
     override func updateUIAfterLoginLogoutAction() {
         super.updateUIAfterLoginLogoutAction()
-        if !UserProfileHelper.isLoggedIn() {
-            container.hidden = true
-            profileImage.image = UIImage.init(imageLiteral: "avatar")
-            logoutButton.hidden = true
-        } else {
+
+        if UserProfileHelper.isLoggedIn() {
             container.hidden = false
             logoutButton.hidden = false
+
+            UserProfileHelper.getUser() { user, error -> () in
+                if let user = user {
+                    // TODO: get name, username etc.
+                    ImageHelper.loadImageFromURL(user.visual, toImageView: self.profileImage)
+                }
+                // TODO: Error handling.
+            }
+        } else {
+            container.hidden = true
+            logoutButton.hidden = true
+            profileImage.image = UIImage(named: "avatar")
         }
     }
 

f399fa61464a6506ecf5b33438415d70e03ae1ab
Bjarne Sievers <jellyfishcoding@live.de>
2016-07-17 21:05:38 +0200

    Added date label in news table view

--- xikolo-ios/Views/NewsArticleCell.swift
+++ xikolo-ios/Views/NewsArticleCell.swift
@@ -13,9 +13,18 @@ class NewsArticleCell : UITableViewCell {
     @IBOutlet weak var titleView: UILabel!
     @IBOutlet weak var descriptionView: UILabel!
     @IBOutlet weak var readStateView: UIView!
+    @IBOutlet weak var dateView: UILabel!
 
     func configure(newsArticle: NewsArticle) {
         readStateView.backgroundColor = Brand.TintColor
+
+        if let date = newsArticle.published_at {
+            let dateFormatter = NSDateFormatter()
+            dateFormatter.dateStyle = .MediumStyle
+            dateFormatter.timeStyle = .NoStyle
+            dateView.text = dateFormatter.stringFromDate(date)
+        }
+
         titleView.text = newsArticle.title
         descriptionView.text = newsArticle.text
     }

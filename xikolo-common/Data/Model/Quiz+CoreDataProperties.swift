d6cad82d1894b47f8227ce2191343d3204ec3d8f
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2016-07-25 17:06:03 +0200

    Added attributes to quiz model

--- xikolo-common/Data/Model/Quiz+CoreDataProperties.swift
+++ xikolo-common/Data/Model/Quiz+CoreDataProperties.swift
@@ -2,7 +2,7 @@
 //  Quiz+CoreDataProperties.swift
 //  xikolo-ios
 //
-//  Created by Sebastian Brückner on 02.06.16.
+//  Created by Sebastian Brückner on 25.07.16.
 //  Copyright © 2016 HPI. All rights reserved.
 //
 //  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
@@ -14,5 +14,12 @@ import CoreData
 
 extension Quiz {
 
+    @NSManaged var instructions: String?
+    @NSManaged var lock_submissions_at: NSDate?
+    @NSManaged var publish_results_at: NSDate?
+    @NSManaged var show_welcome_page_int: NSNumber?
+    @NSManaged var time_limit: NSNumber?
+    @NSManaged var allowed_attempts: NSNumber?
+    @NSManaged var max_points: NSDecimalNumber?
 
 }
--- xikolo-common/Data/Model/Quiz.swift
+++ xikolo-common/Data/Model/Quiz.swift
@@ -12,6 +12,15 @@ import Spine
 
 class Quiz : Content {
 
+    var show_welcome_page: Bool {
+        get {
+            return show_welcome_page_int?.boolValue ?? false
+        }
+        set(new_show_welcome_page) {
+            show_welcome_page_int = new_show_welcome_page
+        }
+    }
+
     override func iconName() -> String {
         return "quiz"
     }
@@ -19,17 +28,35 @@ class Quiz : Content {
 }
 
 class QuizSpine : ContentSpine {
-    
+
+    var instructions: String?
+    var lock_submissions_at: NSDate?
+    var publish_results_at: NSDate?
+    var time_limit: NSNumber?
+    var allowed_attempts: NSNumber?
+    var max_points: NSDecimalNumber?
+    var show_welcome_page_int: NSNumber?
+
+    var questions: ResourceCollection?
+
     override class var cdType: BaseModel.Type {
         return Quiz.self
     }
-    
+
     override class var resourceType: ResourceType {
         return "quizzes"
     }
-    
+
     override class var fields: [Field] {
-        return fieldsFromDictionary([:])
+        return fieldsFromDictionary([
+            "instructions": Attribute(),
+            "lock_submissions_at": DateAttribute(),
+            "publish_results_at": DateAttribute(),
+            "time_limit": Attribute(),
+            "allowed_attempts": Attribute(),
+            "max_points": Attribute(),
+            "show_welcome_page_int": Attribute().serializeAs("show_welcome_page"),
+        ])
     }
-    
+
 }

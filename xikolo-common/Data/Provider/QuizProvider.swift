fb741fd4c4b9fc899444fe15c060417bf73689ea
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2016-08-01 22:01:33 -0400

    Added questions to quiz provider

--- xikolo-common/Data/Provider/QuizProvider.swift
+++ xikolo-common/Data/Provider/QuizProvider.swift
@@ -15,11 +15,23 @@ class QuizProvider {
     class func getQuiz(quizId: String) -> Future<QuizSpine, XikoloError> {
         let spine = SpineModelHelper.createSpineClient()
         spine.registerResource(QuizSpine)
+        spine.registerResource(QuizQuestionSpine)
 
         return spine.findOne(quizId, ofType: QuizSpine.self).map { tuple in
             tuple.resource
         }.mapError { error in
             XikoloError.API(error)
+        }.flatMap { (quiz: QuizSpine) -> Future<QuizSpine, XikoloError> in
+            if let questions = quiz.questions {
+                return spine.find(Query(resourceType: QuizQuestionSpine.self, resourceCollection: questions)).map { resources, _, _ in
+                    quiz.questions = resources
+                    return quiz
+                }.mapError { error in
+                    XikoloError.API(error)
+                }
+            } else {
+                return Future.init(error: XikoloError.InvalidData)
+            }
         }
     }
 

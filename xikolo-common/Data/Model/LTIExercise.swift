97aa422a2bc6dbd0ffb44fdf2f2d7503c4117b44
Bjarne Sievers <jellyfishcoding@live.de>
2016-08-20 16:16:25 +0200

    Added LTIExercise and PeerAssessment models

---
+++ xikolo-common/Data/Model/LTIExercise.swift
@@ -0,0 +1,45 @@
+//
+//  LTIExercise.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 20.08.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import CoreData
+import Foundation
+import Spine
+
+class LTIExercise : Content {
+
+    override func iconName() -> String {
+        return "lti_exercise"
+    }
+
+}
+
+class LTIExerciseSpine : ContentSpine {
+
+    var instructions: String?
+    var weight: NSNumber?
+    var allowed_attempts: NSNumber?
+    var lock_submissions_at: NSDate?
+
+    override class var cdType: BaseModel.Type {
+        return LTIExercise.self
+    }
+
+    override class var resourceType: ResourceType {
+        return "lti-exercises"
+    }
+
+    override class var fields: [Field] {
+        return fieldsFromDictionary([
+            "instructions": Attribute(),
+            "weight": Attribute(),
+            "allowed_attempts": Attribute(),
+            "lock_submissions_at": DateAttribute(),
+        ])
+    }
+    
+}
---
+++ xikolo-common/Data/Model/PeerAssessment+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  PeerAssessment+CoreDataProperties.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 20.08.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension PeerAssessment {
+
+    @NSManaged var title: String?
+
+}

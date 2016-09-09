6a486f7bd443e62a7c46052be873ccc7b42ad9a8
Bjarne Sievers <jellyfishcoding@live.de>
2016-09-09 11:58:40 +0200

    Added PlatformEvent to model

---
+++ xikolo-common/Data/Model/PlatformEvent.swift
@@ -0,0 +1,44 @@
+//
+//  PlatformEvent.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 07.09.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import Spine
+
+
+class PlatformEvent : BaseModel {
+
+}
+
+class PlatformEventSpine : BaseModelSpine {
+
+    var created_at: NSDate?
+    var preview: String?
+    var title: String?
+    var type: String?
+
+    var course: CourseSpine?
+
+    override class var cdType: BaseModel.Type {
+        return PlatformEvent.self
+    }
+
+    override class var resourceType: ResourceType {
+        return "platform-events"
+    }
+
+    override class var fields: [Field] {
+        return fieldsFromDictionary([
+            "title": Attribute(),
+            "type": Attribute(),
+            "created_at": DateAttribute(),
+            "preview": Attribute(),
+            "course": ToOneRelationship(CourseSpine),
+        ])
+    }
+}

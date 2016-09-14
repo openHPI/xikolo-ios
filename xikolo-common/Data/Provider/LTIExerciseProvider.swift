1f8d0e6121b7c84a3509b07e974ec9911f604223
Bjarne Sievers <jellyfishcoding@live.de>
2016-09-14 18:50:34 +0200

    Added lti exercise and peer assessment provider

---
+++ xikolo-common/Data/Provider/LTIExerciseProvider.swift
@@ -0,0 +1,18 @@
+//
+//  LTIExerciseProvider.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 13.09.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import BrightFutures
+import Foundation
+
+class LTIExerciseProvider {
+
+    class func getLTIExercise(ltiExerciseId: String) -> Future<LTIExerciseSpine, XikoloError> {
+        return SpineHelper.findOne(ltiExerciseId, ofType: LTIExerciseSpine.self)
+    }
+
+}
---
+++ xikolo-common/Data/Provider/PeerAssessmentProvider.swift
@@ -0,0 +1,18 @@
+//
+//  PeerAssessmentProvider.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 13.09.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import BrightFutures
+import Foundation
+
+class PeerAssessmentProvider {
+
+    class func getPeerAssessment(peerAssessmentId: String) -> Future<PeerAssessmentSpine, XikoloError> {
+        return SpineHelper.findOne(peerAssessmentId, ofType: PeerAssessmentSpine.self)
+    }
+    
+}

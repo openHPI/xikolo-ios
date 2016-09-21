5cdea7f3647ed1259171bbe7f7ff3eb253dc0206
Bjarne Sievers <jellyfishcoding@live.de>
2016-09-22 00:15:08 +0200

    Added status label to course cell. Fixes #66

---
+++ xikolo-common/Branding/Brand+generic.swift
@@ -0,0 +1,17 @@
+//
+//  Brand+generic.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 08.12.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import UIKit
+
+extension Brand {
+
+    static let FlagUpcomingColor = UIColor.init(red: 20/255, green: 136/255, blue: 255/255, alpha: 1.0)
+    static let FlagSelfpacedColor = UIColor.init(red: 245/255, green: 167/255, blue: 4/255, alpha: 1.0)
+    static let FlagRunningColor = UIColor.init(red: 140/255, green: 179/255, blue: 13/255, alpha: 1.0)
+
+}

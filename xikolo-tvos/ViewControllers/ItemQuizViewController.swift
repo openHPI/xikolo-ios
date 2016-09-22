0ca453792f892df230696e717fd9f286e0b6adf1
Sebastian Brückner <sebastian.brueckner@student.hpi.uni-potsdam.de>
2016-09-22 16:17:01 +0200

    Implemented proper quiz behaviour with correctness data

--- xikolo-tvos/ViewControllers/ItemQuizViewController.swift
+++ xikolo-tvos/ViewControllers/ItemQuizViewController.swift
@@ -106,29 +106,17 @@ class ItemQuizViewController : UIViewController {
     }
 
     func updateCurrentQuestion() {
-        updateButtons()
         updateQuestionView()
+        updateButtons()
+        setNeedsFocusUpdate()
         indicatorView.activeQuestion = questions[currentQuestion]
     }
 
     func updateQuestionView() {
         // TODO: Animation?
         if let vc = questionViewController {
-            if submissionMode! != .ShowSubmission {
-                vc.saveSubmission()
-                if vc.question.submission != nil {
-                    let submission = quiz.submission!, questions = self.questions
-                    let saveOperation = { () -> Future<QuizSubmission, XikoloError> in
-                        let errorMessage = NSLocalizedString("Your progress could not be saved online. Please try again later.", comment: "Your progress could not be saved online. Please try again later.")
-                        return QuizHelper.saveSubmission(submission, questions: questions).onFailure(callback: self.handleError(errorMessage))
-                    }
-                    if currentSaveOperation == nil {
-                        currentSaveOperation = saveOperation()
-                    } else {
-                        // Make sure the save operations happen serially.
-                        currentSaveOperation = currentSaveOperation.flatMap { _ in saveOperation() }
-                    }
-                }
+            if !vc.question.hasCorrectnessData && !vc.readOnly {
+                saveProgress(vc)
             }
 
             vc.removeChildViewControllerFromParent()
@@ -138,7 +126,7 @@ class ItemQuizViewController : UIViewController {
         let question = questions[currentQuestion]
 
         let vc = viewControllerForQuestion(question)
-        vc.readOnly = submissionMode! == .ShowSubmission
+        vc.readOnly = submissionMode! == .ShowSubmission || (question.hasCorrectnessData && question.submission != nil)
 
         addChildViewController(vc, into: questionView)
 
@@ -151,6 +139,23 @@ class ItemQuizViewController : UIViewController {
         }
 
         questionViewController = vc
+    }
+
+    private func saveProgress(vc: AbstractQuestionViewController) {
+        vc.saveSubmission()
+        if vc.question.submission != nil {
+            let submission = quiz.submission!, questions = self.questions
+            let saveOperation = { () -> Future<QuizSubmission, XikoloError> in
+                let errorMessage = NSLocalizedString("Your progress could not be saved online. Please try again later.", comment: "Your progress could not be saved online. Please try again later.")
+                return QuizHelper.saveSubmission(submission, questions: questions).onFailure(callback: self.handleError(errorMessage))
+            }
+            if currentSaveOperation == nil {
+                currentSaveOperation = saveOperation()
+            } else {
+                // Make sure the save operations happen serially.
+                currentSaveOperation = currentSaveOperation.flatMap { _ in saveOperation() }
+            }
+        }
     }
 
     private func viewControllerForQuestion(question: QuizQuestion) -> AbstractQuestionViewController {
@@ -171,18 +176,28 @@ class ItemQuizViewController : UIViewController {
     }
 
     @IBAction func nextQuestion(sender: UIButton) {
-        if currentQuestion == questions.count - 1 {
-            // Save current question.
-            questionViewController?.saveSubmission()
-
-            // TODO: Check if all questions have been answered, warn the user otherwise.
-
-            quiz.submission!.submitted = true
-            let errorMessage = NSLocalizedString("The quiz could not be submitted. Please try again later.", comment: "The quiz could not be submitted. Please try again later.")
-            QuizHelper.saveSubmission(quiz.submission!, questions: questions).onFailure(callback: handleError(errorMessage))
-            // TODO: Success handling.
+        let vc = questionViewController!
+        if vc.question.hasCorrectnessData && !vc.readOnly {
+            saveProgress(vc)
+            if vc.question.submission != nil {
+                vc.readOnly = true
+            }
+            updateButtons()
         } else {
-            currentQuestion += 1
+            if currentQuestion == questions.count - 1 {
+                // TODO: Check if all questions have been answered, warn the user otherwise.
+
+                quiz.submission!.submitted = true
+                let errorMessage = NSLocalizedString("The quiz could not be submitted. Please try again later.", comment: "The quiz could not be submitted. Please try again later.")
+                QuizHelper.saveSubmission(quiz.submission!, questions: questions)
+                    .onFailure(callback: handleError(errorMessage))
+                    .onFailure { _ in
+                        self.quiz.submission!.submitted = false
+                    }
+                // TODO: Success handling.
+            } else {
+                currentQuestion += 1
+            }
         }
     }
 
@@ -192,12 +207,13 @@ class ItemQuizViewController : UIViewController {
         } else {
             previousButton.hidden = false
         }
-        if currentQuestion == questions.count - 1 {
+        if questions[currentQuestion].hasCorrectnessData && !questionViewController!.readOnly {
+            nextButton.setTitle(NSLocalizedString("Submit", comment: "Submit"), forState: .Normal)
+        } else if currentQuestion == questions.count - 1 {
             nextButton.setTitle(NSLocalizedString("Finish", comment: "Finish"), forState: .Normal)
         } else {
             nextButton.setTitle(NSLocalizedString("Next", comment: "Next"), forState: .Normal)
         }
-        setNeedsFocusUpdate()
     }
 
 }

b9f2c2d1ee892287edac1f8920ee4488c25cf212
Bjarne Sievers <jellyfishcoding@live.de>
2016-12-09 20:37:17 +0100

    Added empty state to views

--- xikolo-ios/ViewControllers/News/PlatformEventsTableViewController.swift
+++ xikolo-ios/ViewControllers/News/PlatformEventsTableViewController.swift
@@ -8,11 +8,17 @@
 
 import CoreData
 import UIKit
+import DZNEmptyDataSet
 
 class PlatformEventsTableViewController: UITableViewController {
 
     var resultsController: NSFetchedResultsController!
     var resultsControllerDelegateImplementation: TableViewResultsControllerDelegateImplementation!
+
+    deinit {
+        self.tableView?.emptyDataSetSource = nil
+        self.tableView?.emptyDataSetDelegate = nil
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +36,17 @@ class PlatformEventsTableViewController: UITableViewController {
         } catch {
             // TODO: Error handling.
         }
+        PlatformEventHelper.syncPlatformEvents().onComplete { _ in
+                self.tableView.reloadEmptyDataSet()
+        }
+        setupEmptyState()
+    }
+
+    func setupEmptyState() {
+        tableView.emptyDataSetSource = self
+        tableView.emptyDataSetDelegate = self
+        tableView.tableFooterView = UIView()
+        tableView.reloadEmptyDataSet()
     }
 
 }
@@ -42,5 +59,31 @@ extension PlatformEventsTableViewController : TableViewResultsControllerDelegate
         let event = resultsController.objectAtIndexPath(indexPath) as! PlatformEvent
         cell.configure(event)
     }
-    
+
+}
+
+extension PlatformEventsTableViewController : DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
+
+    func titleForEmptyDataSet(scrollView: UIScrollView!) -> NSAttributedString! {
+        let title: String
+        if UserProfileHelper.isLoggedIn() {
+            title = NSLocalizedString("There has been no course activity yet", comment: "")
+        } else {
+            title = NSLocalizedString("Please log in to see course activity", comment: "")
+        }
+        let attributedString = NSAttributedString(string: title)
+        return attributedString
+    }
+
+    func descriptionForEmptyDataSet(scrollView: UIScrollView!) -> NSAttributedString! {
+        let description: String
+        if UserProfileHelper.isLoggedIn() {
+            description = NSLocalizedString("Notifications about course material or discussions of enrolled courses will appear here", comment: "")
+        } else {
+            description = NSLocalizedString("Course activity is specific to your courses so we need to now what you're enrolled in", comment: "")
+        }
+        let attributedString = NSAttributedString(string: description)
+        return attributedString
+    }
+
 }

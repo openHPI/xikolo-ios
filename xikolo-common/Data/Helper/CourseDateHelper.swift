86330bc2541df700de53732af5003abdf200db8d
Bjarne Sievers <jellyfishcoding@live.de>
2016-12-19 19:41:27 +0100

    Added course date model, provider and helper

---
+++ xikolo-common/Data/Helper/CourseDateHelper.swift
@@ -0,0 +1,49 @@
+//
+//  CourseDateHelper.swift
+//  xikolo-ios
+//
+//  Created by Tobias Rohloff on 15.11.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import BrightFutures
+import CoreData
+
+class CourseDateHelper {
+
+    static func getCourseDatesRequest() -> NSFetchRequest {
+        let request = NSFetchRequest(entityName: "CourseDate")
+        let dateSort = NSSortDescriptor(key: "date", ascending: false)
+        request.sortDescriptors = [dateSort]
+        return request
+    }
+
+    static func getCourseDeadlinesRequest() -> NSFetchRequest {
+        let request = NSFetchRequest(entityName: "CourseDate")
+        let courseSort = NSSortDescriptor(key: "course.title", ascending: true)
+        let dateSort = NSSortDescriptor(key: "date", ascending: false)
+        let predicate = NSPredicate(format: "type != 'course_start'")
+        request.predicate = predicate
+        request.sortDescriptors = [courseSort, dateSort]
+        return request
+    }
+
+    static func getCourseStartsRequest() -> NSFetchRequest {
+        let request = NSFetchRequest(entityName: "CourseDate")
+        let dateSort = NSSortDescriptor(key: "date", ascending: false)
+        let predicate = NSPredicate(format: "type = 'course_start'")
+        request.predicate = predicate
+        request.sortDescriptors = [dateSort]
+        return request
+    }
+
+    static func syncCourseDates() -> Future<[CourseDate], XikoloError> {
+        return CourseDateProvider.getCourseDates().flatMap { spineCourseDates -> Future<[BaseModel], XikoloError> in
+            let request = getCourseDatesRequest()
+            return SpineModelHelper.syncObjectsFuture(request, spineObjects: spineCourseDates, inject: nil, save: true)
+            }.map { cdCourseDates in
+                return cdCourseDates as! [CourseDate]
+        }
+    }
+
+}
---
+++ xikolo-common/Data/Model/CourseDate.swift
@@ -0,0 +1,43 @@
+//
+//  CourseDate.swift
+//  xikolo-ios
+//
+//  Created by Tobias Rohloff on 09.11.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import Spine
+
+
+class CourseDate : BaseModel {
+
+}
+
+class CourseDateSpine : BaseModelSpine {
+
+    var type: String?
+    var title: String?
+    var date: NSDate?
+
+    var course: CourseSpine?
+
+    override class var resourceType: ResourceType {
+        return "course-dates"
+    }
+
+    override class var cdType: BaseModel.Type {
+        return CourseDate.self
+    }
+
+    override class var fields: [Field] {
+        return fieldsFromDictionary([
+            "type": Attribute(),
+            "title": Attribute(),
+            "date": DateAttribute(),
+            "course": ToOneRelationship(CourseSpine)
+        ])
+    }
+
+}

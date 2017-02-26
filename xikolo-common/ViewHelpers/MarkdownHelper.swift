7e0d94c3a53e8ec4534d7d2a0601314b87590d68
Sebastian Brückner <mail@invlid.com>
2017-02-25 21:48:37 -0800

    Use Down on tvOS (#141)

---
+++ xikolo-common/ViewHelpers/MarkdownHelper.swift
@@ -0,0 +1,44 @@
+//
+//  MarkdownHelper.swift
+//  xikolo-ios
+//
+//  Created by Bjarne Sievers on 18.12.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import UIKit
+import Down
+
+class MarkdownHelper {
+
+    class func parse(_ string: String) throws -> NSMutableAttributedString {
+        let parser = Down(markdownString: string)
+
+        #if os(tvOS)
+            let color = "white"
+            let fontSize = 29
+        #else
+            let color = "black"
+            let fontSize = 14
+        #endif
+
+        if let attributedString = try? parser.toAttributedStringWithFont(font: "-apple-system", fontSize: fontSize, color: color) {
+            let mutableString: NSMutableAttributedString = NSMutableAttributedString(attributedString: attributedString)
+            return mutableString
+        } else {
+            throw XikoloError.markdownError
+        }
+    }
+
+}
+
+public extension DownAttributedStringRenderable {
+    public func toAttributedStringWithFont(_ options: DownOptions = .Default, font: String, fontSize: Int, color: String) throws -> NSAttributedString {
+        let htmlResponse = try self.toHTML(options)
+        let html = "<span style=\"font-family: \(font); font-size: \(fontSize); color: \(color);\">\(htmlResponse)</span>"
+        return try NSAttributedString(data: html.data(using: .utf8)!, options: [
+            NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+            NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue,
+        ], documentAttributes: nil)
+    }
+}

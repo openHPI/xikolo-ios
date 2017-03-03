89ef78623ae4413aa3e4d6ca459a16ca75a59c3d
Sebastian Brückner <mail@invlid.com>
2017-03-02 23:32:24 -0800

    Removed staging. (#150)

--- xikolo-common/Branding/openHPI/Brand.swift
+++ xikolo-common/Branding/openHPI/Brand.swift
@@ -11,11 +11,7 @@ import UIKit
 
 struct Brand {
 
-    #if DEBUG
-    static let BaseURL = "https://staging.openhpi.de"
-    #else
     static let BaseURL = "https://open.hpi.de"
-    #endif
 
     static let TintColor = UIColor.init(red: 222/255, green: 98/255, blue: 18/255, alpha: 1.0)
     static let AppID = "de.xikolo.openhpi"

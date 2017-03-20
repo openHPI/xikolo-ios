3097906ed29d024d732394b1a50fce0bb9fc0270
Bjarne Sievers <jellyfishcoding@live.de>
2017-03-20 15:12:04 +0100

    Refactored predicate logic of course helper

--- xikolo-common/Data/Helper/CourseHelper.swift
+++ xikolo-common/Data/Helper/CourseHelper.swift
@@ -12,41 +12,68 @@ import CoreData
 class CourseHelper {
 
     static fileprivate let entity = NSEntityDescription.entity(forEntityName: "Course", in: CoreDataHelper.managedContext)!
+    static fileprivate let genericPredicate = NSPredicate(format: "external_int != true")
+    static fileprivate let enrolledPredicate = NSPredicate(format: "external_int != true AND enrollment != null")
+    static fileprivate let unenrolledPredicate = NSPredicate(format: "external_int != true AND enrollment == null")
+    static fileprivate let announcedPredicate = NSPredicate(format: "status = %@", "announced")
+    static fileprivate let previewPredicate = NSPredicate(format: "status = %@", "preview")
+    static fileprivate let activePredicate = NSPredicate(format: "status = %@", "active")
+    static fileprivate let selfpacedPredicate = NSPredicate(format: "status = %@", "self-paced")
+    static fileprivate let interestingPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [announcedPredicate,previewPredicate,activePredicate])
+    static fileprivate let accessiblePredicate = NSPredicate(format: "accessible_int == true")
+    static fileprivate let completedPredicate = NSPredicate(format: "enrollment.completed_int == true")
+    static fileprivate let notcompletedPredicate = NSPredicate(format: "enrollment.completed_int != true")
 
     static func getGenericCoursesRequest() -> NSFetchRequest<NSFetchRequestResult> {
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Course")
         let customOrderSort = NSSortDescriptor(key: "order", ascending: true)
         request.sortDescriptors = [customOrderSort]
+        request.predicate = genericPredicate
         return request
     }
 
     static func getAllCoursesRequest() -> NSFetchRequest<NSFetchRequestResult> {
-        let request = getGenericCoursesRequest()
-        request.predicate = NSPredicate(format: "external_int != true")
-        return request
+        return getGenericCoursesRequest()
     }
 
     static func getUnenrolledCoursesRequest() -> NSFetchRequest<NSFetchRequestResult> {
         let request = getGenericCoursesRequest()
-        request.predicate = NSPredicate(format: "external_int != true AND hidden_int != true AND enrollment == null")
+        request.predicate = unenrolledPredicate
         return request
     }
 
     static func getEnrolledCoursesRequest() -> NSFetchRequest<NSFetchRequestResult> {
         let request = getGenericCoursesRequest()
-        request.predicate = NSPredicate(format: "external_int != true AND enrollment != null")
+        request.predicate = enrolledPredicate
+        return request
+    }
+
+    static func getInterestingCoursesRequest() -> NSFetchRequest<NSFetchRequestResult> {
+        let request = getGenericCoursesRequest()
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [unenrolledPredicate, interestingPredicate])
+        return request
+    }
+
+    static func getPastCoursesRequest() -> NSFetchRequest<NSFetchRequestResult> { // unenrolled: self-paced
+        let request = getGenericCoursesRequest()
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [unenrolledPredicate, selfpacedPredicate])
         return request
     }
 
     static func getEnrolledAccessibleCoursesRequest() -> NSFetchRequest<NSFetchRequestResult> {
         let request = getGenericCoursesRequest()
-        request.predicate = NSPredicate(format: "external_int != true AND enrollment != null AND accessible_int == true")
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [enrolledPredicate, accessiblePredicate, notcompletedPredicate])
+        return request
+    }
+
+    static func getCompletedCoursesRequest() -> NSFetchRequest<NSFetchRequestResult> {
+        let request = getGenericCoursesRequest()
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [enrolledPredicate, completedPredicate])
         return request
     }
 
     static func getSectionedRequest() -> NSFetchRequest<NSFetchRequestResult> {
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Course")
-        request.predicate = NSPredicate(format: "external_int != true")
+        let request = getGenericCoursesRequest()
         let enrolledSort = NSSortDescriptor(key: "enrollment", ascending: false)
         let startDateSort = NSSortDescriptor(key: "start_at", ascending: false)
         request.sortDescriptors = [enrolledSort, startDateSort]

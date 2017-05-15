a4ca39a85538be62183bc870b6f361f68b447713
Bjarne Sievers <jellyfishcoding@live.de>
2017-05-15 15:07:50 +0200

    Add dedicated link handling for markdown views

--- xikolo-ios/ViewControllers/Courses/RichtextViewController.swift
+++ xikolo-ios/ViewControllers/Courses/RichtextViewController.swift
@@ -7,7 +7,39 @@
 //
 
 import UIKit
+import SafariServices
 
 class RichtextViewController : AbstractItemRichtextViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textView.delegate = self
+    }
+
+}
+
+extension RichtextViewController: UITextViewDelegate {
+
+    func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
+        var url = URL
+        if (URL.scheme == "applewebdata") { // replace applewebdata with baseURL for relative urls in markdown
+            var absoluteString = URL.absoluteString
+            let trimmedUrlString = absoluteString.stringByRemovingRegexMatches(pattern: "^(?:applewebdata://[0-9A-Z-]*/?)", replaceWith: Brand.BaseURL + "/")
+            guard let trimmedString = trimmedUrlString else { return false }
+            guard let trimmedURL = getURL(forString: trimmedString) else { return false }
+            url = trimmedURL
+        }
+        if !(url.scheme?.hasPrefix("http") ?? false) { // abort if it still isnt http
+            return false
+        }
+        let safariVC = SFSafariViewController(url: url)
+        present(safariVC, animated: true, completion: nil)
+        safariVC.preferredControlTintColor = Brand.TintColor
+        return false
+    }
+
+    func getURL(forString string: String) -> URL? {
+        return URL(string: string) // necessary because someone clever put the argument in CAPS in the function above
+    }
 
 }

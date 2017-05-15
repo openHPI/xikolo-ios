9113cd0e9cd0b74b3257f2532419080565796ccf
Bjarne Sievers <jellyfishcoding@live.de>
2017-05-16 01:12:10 +0200

    Add empty state for course-dates tableview

--- xikolo-ios/ViewControllers/Dashboard/CourseDatesTableViewController.swift
+++ xikolo-ios/ViewControllers/Dashboard/CourseDatesTableViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import DZNEmptyDataSet
 
 class CourseDatesTableViewController : UITableViewController {
 
@@ -15,6 +16,11 @@ class CourseDatesTableViewController : UITableViewController {
     var resultsControllerDelegateImplementation: TableViewResultsControllerDelegateImplementation!
 
     weak var delegate: CourseDatesTableViewControllerDelegate?
+
+    deinit {
+        self.tableView?.emptyDataSetSource = nil
+        self.tableView?.emptyDataSetDelegate = nil
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,6 +39,14 @@ class CourseDatesTableViewController : UITableViewController {
         } catch {
             // TODO: Error handling.
         }
+        setupEmptyState()
+    }
+
+    func setupEmptyState() {
+        tableView.emptyDataSetSource = self
+        tableView.emptyDataSetDelegate = self
+        tableView.tableFooterView = UIView()
+        tableView.reloadEmptyDataSet()
     }
 
     override func viewDidLayoutSubviews() {
@@ -56,7 +70,11 @@ class CourseDatesTableViewController : UITableViewController {
 
     func tableViewHeight() -> CGFloat {
         tableView.layoutIfNeeded()
-        return tableView.contentSize.height
+        if tableView.contentSize.height == 0.0 {
+            return 280
+        } else {
+            return tableView.contentSize.height
+        }
     }
 
 }
@@ -77,6 +95,38 @@ extension CourseDatesTableViewController : TableViewResultsControllerDelegateImp
             AppDelegate.instance().goToCourse(course)
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+}
+
+extension CourseDatesTableViewController : DZNEmptyDataSetSource, DZNEmptyDataSetDelegate {
+
+    func title(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
+        if NetworkIndicator.counter > 0 {
+            return nil // blank screen for loading
+        }
+        let title: String
+        if UserProfileHelper.isLoggedIn() {
+            title = NSLocalizedString("There are no important course dates yet", comment: "")
+        } else {
+            title = NSLocalizedString("Please log in to see your personal deadlines", comment: "")
+        }
+        let attributedString = NSAttributedString(string: title)
+        return attributedString
+    }
+
+    func description(forEmptyDataSet scrollView: UIScrollView!) -> NSAttributedString! {
+        if NetworkIndicator.counter > 0 {
+            return nil // blank screen for loading
+        }
+        let description: String
+        if UserProfileHelper.isLoggedIn() {
+            description = NSLocalizedString("Notifications about deadlines or new courses will appear here", comment: "")
+        } else {
+            description = NSLocalizedString("Course dates and deadlines are specific to your courses so we need to now what you're enrolled in", comment: "")
+        }
+        let attributedString = NSAttributedString(string: description)
+        return attributedString
     }
     
 }

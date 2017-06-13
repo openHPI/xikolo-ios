35b3cd255ecd560e96bee89df3559d580699eb5b
Bjarne Sievers <jellyfishcoding@live.de>
2017-06-14 01:12:20 +0200

    Add mooc house targets

---
+++ xikolo-common/Branding/moocHOUSE/Brand.swift
@@ -0,0 +1,26 @@
+//
+//  Brand.swift
+//  xikolo-ios
+//
+//  Created by Sebastian Brückner on 20.07.16.
+//  Copyright © 2016 HPI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Brand {
+
+    static let BaseURL = "https://mooc.house"
+
+    //TODO: Change colors
+    static let TintColor = UIColor(red: 222/255, green: 98/255, blue: 18/255, alpha: 1.0)
+    static let TintColorSecond = UIColor(red: 180/255, green: 41/255, blue: 70/255, alpha: 1.0)
+    static let TintColorThird = UIColor(red: 245/255, green: 167/255, blue: 4/255, alpha: 1.0)
+    static let AppID = "de.xikolo.moochouse"
+    static let PlatformTitle = "moochouse"
+
+    static let IMPRINT_URL = Brand.BaseURL + "/pages/imprint"
+    static let PRIVACY_URL = Brand.BaseURL + "/pages/privacy"
+
+}
---
+++ xikolo-tvos/Branding/moocHOUSE/Brand+tvOS.swift
@@ -0,0 +1,15 @@
+//
+//  Brand+tvOS.swift
+//  xikolo-ios
+//
+//  Created by BJarne Sievers on 14.06.17.
+//  Copyright © 2017 HPI. All rights reserved.
+//
+
+import Foundation
+
+extension Brand {
+
+    static let AppGroupID = "group.de.xikolo.moochouse"
+
+}

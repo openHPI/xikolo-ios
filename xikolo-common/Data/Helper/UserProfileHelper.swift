118c1ef6b561dca9d003abe4c6b0f17e7bef68d3
Bjarne Sievers <jellyfishcoding@live.de>
2017-07-23 13:38:09 +0200

    Only refresh enrollments on login since they contain all necessary relationships

--- xikolo-common/Data/Helper/UserProfileHelper.swift
+++ xikolo-common/Data/Helper/UserProfileHelper.swift
@@ -95,7 +95,7 @@ open class UserProfileHelper {
 
     static func refreshUserDependentData() {
         UserHelper.syncMe()
-        CourseHelper.refreshCourses()
+        EnrollmentHelper.syncEnrollments()
         CourseDateHelper.syncCourseDates()
         AnnouncementHelper.syncAnnouncements()
     }

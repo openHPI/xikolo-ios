80650c8b2de3b8a6649fc00db3294fb81e0e1644
Max Bothe <max.bothe@hpi.de>
2017-07-24 15:10:53 +0200

    Improve notification keys

--- xikolo-common/NotificationKeys.swift
+++ xikolo-common/NotificationKeys.swift
@@ -8,10 +8,8 @@
 
 import Foundation
 
-class NotificationKeys: NSObject {
+struct NotificationKeys {
 
-//    static let loginSuccessfulKey = Notification.Name("de.hpi.open.loginsuccessful")
-//    static let logoutSuccessfulKey = Notification.Name("de.hpi.open.logoutsuccessful")
     static let loginStateChangedKey = Notification.Name("de.hpi.open.loginStateChanged")
 
     static let createdEnrollmentKey = Notification.Name("de.hpi.open.createdEnrollment")

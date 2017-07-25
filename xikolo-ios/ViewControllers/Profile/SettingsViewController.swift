43a9600333acab5994b1aec19eee770807c7d17f
Max Bothe <mathebox@gmail.com>
2017-07-25 21:20:53 +0200

    Use tableview for settings tab. (#264)
    * Use tableview for settings tab.
    
    * Add german localization.

--- xikolo-ios/ViewControllers/Profile/SettingsViewController.swift
+++ xikolo-ios/ViewControllers/Profile/SettingsViewController.swift
@@ -7,31 +7,29 @@
 //
 
 import UIKit
+import SafariServices
 import SDWebImage
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UITableViewController {
+
+    static let logoutIndexPath = IndexPath(row: 0, section: 1)
 
     @IBOutlet var loginButton: UIBarButtonItem!
 
     @IBOutlet weak var headerImage: UIImageView!
     @IBOutlet weak var profileImage: UIImageView!
-
     @IBOutlet weak var nameView: UILabel!
     @IBOutlet weak var emailView: UILabel!
-    @IBOutlet weak var logoutButton: UIButton!
+
     @IBOutlet weak var versionView: UILabel!
     @IBOutlet weak var buildView: UILabel!
-
-    @IBAction func logout(_ sender: UIButton) {
-        UserProfileHelper.logout()
-    }
 
     var user: UserProfile?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        versionView.text = NSLocalizedString("Version", comment: "app version") + ": " + UIApplication.appVersion()
-        buildView.text = NSLocalizedString("Build", comment: "app version") + ": " + UIApplication.appBuild()
+        self.versionView.text = NSLocalizedString("Version", comment: "app version") + ": " + UIApplication.appVersion()
+        self.buildView.text = NSLocalizedString("Build", comment: "app version") + ": " + UIApplication.appBuild()
 
         self.updateUIAfterLoginStateChanged()
         NotificationCenter.default.addObserver(self,
@@ -43,17 +41,23 @@ class SettingsViewController: UIViewController {
     func updateUIAfterLoginStateChanged() {
         if UserProfileHelper.isLoggedIn() {
             self.navigationItem.rightBarButtonItem = nil
-            nameView.isHidden = false
-            emailView.isHidden = false
-            logoutButton.isHidden = false
+            self.updateProfileInfo()
+
+            self.profileImage.isHidden = false
+            self.nameView.isHidden = false
+            self.emailView.isHidden = false
+
             UserHelper.syncMe().onSuccess(callback: { _ in self.updateProfileInfo() })
-            updateProfileInfo()
         } else {
             self.navigationItem.rightBarButtonItem = self.loginButton
-            nameView.isHidden = true
-            emailView.isHidden = true
-            logoutButton.isHidden = true
+
+            self.profileImage.isHidden = true
+            self.nameView.isHidden = true
+            self.emailView.isHidden = true
         }
+
+
+        self.tableView.reloadData()
     }
 
     func updateProfileInfo() {
@@ -62,6 +66,49 @@ class SettingsViewController: UIViewController {
         self.emailView.text = userProfile.email
         self.nameView.text = (userProfile.first_name ?? "") + " " + (userProfile.last_name ?? "")
         self.profileImage.sd_setImage(with: user.avatar_url)
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch (indexPath.section, indexPath.item) {
+        case (0, 0):
+            if let url = URL(string: Brand.APP_IMPRINT_URL) {
+                let safariVC = SFSafariViewController(url: url)
+                present(safariVC, animated: true, completion: nil)
+                safariVC.preferredControlTintColor = Brand.TintColor
+            }
+        case (0, 1):
+            if let url = URL(string: Brand.APP_PRIVACY_URL) {
+                let safariVC = SFSafariViewController(url: url)
+                present(safariVC, animated: true, completion: nil)
+                safariVC.preferredControlTintColor = Brand.TintColor
+            }
+        case (1, 0):
+            UserProfileHelper.logout()
+        default:
+            break
+        }
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        let numberOfSections = super.numberOfSections(in: tableView)
+        return UserProfileHelper.isLoggedIn() ? numberOfSections : numberOfSections - 1
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        // Dynamic sizing for the header view
+        if let headerView = self.tableView.tableHeaderView {
+            let height: CGFloat = UserProfileHelper.isLoggedIn() ? 260 : 190
+            var headerFrame = headerView.frame
+
+            // Don't set the same height again to prevent a infinte loop hang.
+            if height != headerFrame.size.height {
+                headerFrame.size.height = height
+                headerView.frame = headerFrame
+                self.tableView.tableHeaderView = headerView
+            }
+        }
     }
 
 }

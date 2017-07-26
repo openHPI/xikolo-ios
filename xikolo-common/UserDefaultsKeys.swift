0d3508197fe11975ead2c629424c6e995ea165f9
Max Bothe <max.bothe@hpi.de>
2017-07-26 09:47:00 +0200

    Move existing key to new UserDefaultsKeys.

--- xikolo-common/UserDefaultsKeys.swift
+++ xikolo-common/UserDefaultsKeys.swift
@@ -14,4 +14,6 @@ struct UserDefaultsKeys {
     // since UserDefault will return `false` if no value for the key is present
     static let noContentPreloadKey = "de.xikolo.ios.content.noPreload"
 
+    static let playbackRateKey = "de.xikolo.ios.video.playbackRate"
+
 }

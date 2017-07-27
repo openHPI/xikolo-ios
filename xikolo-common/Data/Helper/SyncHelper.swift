7803e60cb3d8f19e065b7ed60173ef120d4367d5
Jan Renz <jan.renz@gmail.com>
2017-07-27 14:37:24 +0200

    Removed prints and added TODO

--- xikolo-common/Data/Helper/SyncHelper.swift
+++ xikolo-common/Data/Helper/SyncHelper.swift
@@ -16,32 +16,34 @@ class SyncHelper {
     func startObserving() {
         NotificationCenter.default.addObserver(self, selector: #selector(coreDataChange(note:)), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: CoreDataHelper.viewContext)
          NotificationCenter.default.addObserver(self, selector: #selector(coreDataChange(note:)), name: NSNotification.Name.NSManagedObjectContextObjectsDidChange, object: CoreDataHelper.backgroundContext)
-        print("Start oberserving CoreData")
+        //print("Start oberserving CoreData")
     }
 
     func stopObserving() {
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.NSManagedObjectContextDidSave, object: CoreDataHelper.viewContext)
         NotificationCenter.default.removeObserver(self, name: NSNotification.Name.NSManagedObjectContextDidSave, object: CoreDataHelper.backgroundContext)
-        print("Stop observing CoreData")
+        //print("Stop observing CoreData")
     }
 
+    //support for videos should follow once they contain the seo texts
+    
     @objc func coreDataChange(note: Notification) {
         if let updated = note.userInfo?[NSUpdatedObjectsKey] as? Set<NSManagedObject>, updated.count > 0 {
-            print("updated: \(updated)")
+            //print("updated: \(updated)")
             for case let course as Course in updated {
                 SearchHelper.addCourseToIndex(course: course)
             }
         }
 
         if let deleted = note.userInfo?[NSDeletedObjectsKey] as? Set<NSManagedObject>, deleted.count > 0 {
-            print("deleted: \(deleted)")
+            //print("deleted: \(deleted)")
             for case let course as Course in deleted {
                 SearchHelper.removeCourseFromIndex(course: course)
             }
         }
 
         if let inserted = note.userInfo?[NSInsertedObjectsKey] as? Set<NSManagedObject>, inserted.count > 0 {
-            print("inserted: \(inserted)")
+            //print("inserted: \(inserted)")
             for case let course as Course in inserted {
                 SearchHelper.addCourseToIndex(course: course)
             }

4f3479d3e45035e95b16a27406bfc16b9cb67821
Max Bothe <max.bothe@hpi.de>
2017-08-22 14:28:13 +0200

    Add label for offline state to video player.
    - Refine user interface.
    - Fix iPhone landscape mode.

--- xikolo-ios/Views/VideoPlayerControlView.swift
+++ xikolo-ios/Views/VideoPlayerControlView.swift
@@ -13,26 +13,65 @@ import UIKit
 class VideoPlayerControlView: BMPlayerControlView {
 
     private var playbackRateButton = UIButton(type: .custom)
+    private var offlineLabel = UILabel()
     private(set) var playRate: Float = UserDefaults.standard.float(forKey: UserDefaultsKeys.playbackRateKey)
 
     override func customizeUIComponents() {
+        // update top bar
         self.chooseDefitionView.removeFromSuperview()
 
+        self.topMaskView.addSubview(self.offlineLabel)
+        self.offlineLabel.layer.cornerRadius = 2
+        self.offlineLabel.layer.borderWidth = 1
+        self.offlineLabel.layer.borderColor = UIColor(white: 1.0, alpha: 0.8).cgColor
+        self.offlineLabel.textColor = UIColor(white: 1.0, alpha: 0.9)
+        self.offlineLabel.font = UIFont.systemFont(ofSize: 12)
+        self.offlineLabel.textAlignment = NSTextAlignment.center
+        self.offlineLabel.text = "Offline"
+
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            self.backButton.removeFromSuperview()
+            self.titleLabel.removeFromSuperview()
+
+            self.offlineLabel.snp.makeConstraints { make in
+                make.width.equalTo(50)
+                make.height.equalTo(20)
+                make.top.equalTo(self.topMaskView.snp.top).offset(5)
+                make.right.equalTo(self.topMaskView.snp.right).offset(-20)
+            }
+        } else {
+            self.offlineLabel.snp.makeConstraints { make in
+                make.width.equalTo(50)
+                make.height.equalTo(20)
+                make.centerY.equalTo(self.titleLabel)
+                make.left.equalTo(self.titleLabel.snp.right).offset(5)
+                make.right.equalTo(self.topMaskView.snp.right).offset(-20)
+            }
+        }
+
+        // update bottom bar
         self.bottomMaskView.addSubview(self.playbackRateButton)
 
+        let playbackRateButtonSize = CGSize(width: 40, height: 20)
         self.playRate = self.playRate == 0 ? 1.0 : self.playRate  // playback rate can be 0 on the first time
-        self.playbackRateButton.layer.cornerRadius = 2
-        self.playbackRateButton.layer.borderWidth = 1
-        self.playbackRateButton.layer.borderColor = UIColor(white: 1.0, alpha: 0.8).cgColor
+        let backgroundLayer = CALayer()
+        backgroundLayer.frame = CGRect(x: (44 - playbackRateButtonSize.width)/2,
+                                       y: (50 - playbackRateButtonSize.height)/2,
+                                       width: playbackRateButtonSize.width,
+                                       height: playbackRateButtonSize.height)
+        backgroundLayer.cornerRadius = 2
+        backgroundLayer.borderWidth = 1
+        backgroundLayer.borderColor = UIColor(white: 1.0, alpha: 0.8).cgColor
+        self.playbackRateButton.layer.addSublayer(backgroundLayer)
         self.playbackRateButton.setTitleColor(UIColor(white: 1.0, alpha: 0.9), for: .normal)
         self.playbackRateButton.addTarget(self, action: #selector(onPlaybackRateButtonPressed), for: .touchUpInside)
-        self.playbackRateButton.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+        self.playbackRateButton.titleLabel?.font = UIFont.systemFont(ofSize: 12)
         self.updatePlaybackRateButton()
 
         if UIDevice.current.userInterfaceIdiom == .pad {
             self.playbackRateButton.snp.makeConstraints { make in
-                make.width.equalTo(40)
-                make.height.equalTo(20)
+                make.width.equalTo(44)
+                make.height.equalTo(50)
                 make.centerY.equalTo(self.currentTimeLabel)
                 make.left.equalTo(self.totalTimeLabel.snp.right).offset(5)
                 make.right.equalTo(self.bottomMaskView.snp.right).offset(-10)
@@ -41,8 +80,8 @@ class VideoPlayerControlView: BMPlayerControlView {
             self.fullscreenButton.removeFromSuperview()
         } else {
             self.playbackRateButton.snp.makeConstraints { make in
-                make.width.equalTo(40)
-                make.height.equalTo(20)
+                make.width.equalTo(44)
+                make.height.equalTo(50)
                 make.centerY.equalTo(self.currentTimeLabel)
                 make.left.equalTo(self.totalTimeLabel.snp.right).offset(5)
             }
@@ -56,6 +95,15 @@ class VideoPlayerControlView: BMPlayerControlView {
                 make.right.equalTo(self.bottomMaskView.snp.right)
             }
         }
+    }
+
+    func setOffline(_ isOffline: Bool) {
+        self.offlineLabel.isHidden = !isOffline
+    }
+
+    func changeOrientation(to orientation: UIDeviceOrientation) {
+        self.backButton.isHidden = !orientation.isLandscape
+        self.titleLabel.isHidden = !orientation.isLandscape
     }
 
     @objc private func onPlaybackRateButtonPressed() {

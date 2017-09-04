bbb6180a581865b0ce289bc983dde6c68083b35e
Max Bothe <max.bothe@hpi.de>
2017-09-04 09:47:05 +0200

    Make use of default parameters.

--- xikolo-common/ViewHelpers/Settings.swift
+++ xikolo-common/ViewHelpers/Settings.swift
@@ -13,7 +13,7 @@ class Settings {
     class func open() {
         guard let appSettings = URL(string: UIApplicationOpenSettingsURLString) else { return }
         guard UIApplication.shared.canOpenURL(appSettings) else { return }
-        UIApplication.shared.open(appSettings, options: [:], completionHandler: nil)
+        UIApplication.shared.open(appSettings)
         // TODO: write test for this
     }
 

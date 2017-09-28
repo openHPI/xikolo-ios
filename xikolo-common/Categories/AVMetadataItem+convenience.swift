799a9f455188c08f3dde019cf7c33b1fcc1ec886
Max Bothe <max.bothe@hpi.de>
2017-09-28 16:14:42 +0200

    Refactoring.

--- xikolo-common/Categories/AVMetadataItem+convenience.swift
+++ xikolo-common/Categories/AVMetadataItem+convenience.swift
@@ -11,18 +11,18 @@ import UIKit
 
 extension AVMetadataItem {
 
-    class func item(_ identifier: String, value: String?) -> AVMetadataItem? {
+    class func item(_ identifier: AVMetadataIdentifier, value: String?) -> AVMetadataItem? {
         guard var str = value else {
             return nil
         }
         // HACKHACK: Fix description to prevent visual bug in metadata display.
-        if identifier == AVMetadataIdentifier.commonIdentifierDescription.rawValue && str.characters.count < 212 {
+        if identifier == .commonIdentifierDescription && str.characters.count < 212 {
             str = str + "                                                                                                                                  \n\n\n                                                                                                                                                                                     "
         }
 
         let item = AVMutableMetadataItem()
         item.value = str as NSString
-        item.identifier = AVMetadataIdentifier(rawValue: identifier)
+        item.identifier = identifier
         item.extendedLanguageTag = "und" // Undefined language
         return item.copy() as? AVMetadataItem
     }
--- xikolo-common/Data/Model/Video.swift
+++ xikolo-common/Data/Model/Video.swift
@@ -38,10 +38,10 @@ class Video : Content {
 
     func metadata() -> [AVMetadataItem] {
         var items: [AVMetadataItem] = []
-        if let course_item = self.item, let item = AVMetadataItem.item(AVMetadataIdentifier.commonIdentifierTitle.rawValue, value: course_item.title) {
+        if let course_item = self.item, let item = AVMetadataItem.item(.commonIdentifierTitle, value: course_item.title) {
             items.append(item)
         }
-        if let item = AVMetadataItem.item(AVMetadataIdentifier.commonIdentifierDescription.rawValue, value: summary) {
+        if let item = AVMetadataItem.item(.commonIdentifierDescription, value: summary) {
             items.append(item)
         }
         return items

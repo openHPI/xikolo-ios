5d22db44bcb2e72d27d608498d7af55d30dfcb6d
Maxim Tsvetkov <777.maxxx@gmail.com>
2017-10-29 17:25:11 +0100

    Override AbstractLoginViewController methods to start/stop animating of login button

--- xikolo-ios/ViewControllers/Main/LoginViewController.swift
+++ xikolo-ios/ViewControllers/Main/LoginViewController.swift
@@ -32,6 +32,21 @@ class LoginViewController : AbstractLoginViewController, WKUIDelegate {
             singleSignOnView.isHidden = true
         #endif
     }
+    
+    override  func login() {
+        loginButton.startAnimating()
+        super.login()
+    }
+    
+    override func handleLoginSuccess(with token: String) {
+        loginButton.stopAnimating()
+        super.handleLoginSuccess(with: token)
+    }
+    
+    override func handleLoginFailure(with error: Error) {
+        loginButton.stopAnimating()
+        super.handleLoginFailure(with: error)
+    }
 
     @IBAction func dismiss() {
         presentingViewController?.dismiss(animated: true, completion: nil)

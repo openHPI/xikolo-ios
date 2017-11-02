558e5423b08516de03729a4558d2c4bbf6db6d54
Max Bothe <mathebox@gmail.com>
2017-11-02 10:44:14 +0200

    Fix course enrollment. (#424)
    - The API requires a non-optional value for the completed state.

--- xikolo-common/Data/Model/Enrollment.swift
+++ xikolo-common/Data/Model/Enrollment.swift
@@ -66,6 +66,7 @@ class EnrollmentSpine : BaseModelSpine {
     convenience init(course: CourseSpine){
         self.init()
         self.course = course
+        self.completed_int = 0
         //TODO: What about content
     }
 

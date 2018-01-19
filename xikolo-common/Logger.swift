3d4bc6ee79ab2c418f9110a3f418a283fcb2d71b
Max Bothe <max.bothe@hpi.de>
2018-01-19 16:57:41 +0100

    Add release config

--- xikolo-common/Logger.swift
+++ xikolo-common/Logger.swift
@@ -11,20 +11,30 @@ import XCGLogger
 let log: XCGLogger = {
 
     let log = XCGLogger(identifier: "default", includeDefaultDestinations: false)
-
     let consoleDestination = ConsoleDestination(owner: nil, identifier: "default.consoleDestination")
-    consoleDestination.outputLevel = .verbose
-    consoleDestination.showLogIdentifier = false
-    consoleDestination.showFunctionName = false
-    consoleDestination.showThreadName = false
-    consoleDestination.showLevel = true
-    consoleDestination.showFileName = false
-    consoleDestination.showLineNumber = false
-    consoleDestination.showDate = true
+
+    #if DEBUG
+        consoleDestination.outputLevel = .verbose
+        consoleDestination.showLogIdentifier = false
+        consoleDestination.showFunctionName = false
+        consoleDestination.showThreadName = false
+        consoleDestination.showLevel = true
+        consoleDestination.showFileName = false
+        consoleDestination.showLineNumber = false
+        consoleDestination.showDate = false
+    #else
+        consoleDestination.outputLevel = .severe
+        consoleDestination.showLogIdentifier = true
+        consoleDestination.showFunctionName = true
+        consoleDestination.showThreadName = true
+        consoleDestination.showLevel = true
+        consoleDestination.showFileName = true
+        consoleDestination.showLineNumber = true
+        consoleDestination.showDate = true
+    #endif
 
     log.add(destination: consoleDestination)
     log.logAppDetails()
 
-    log.filters = [FileNameFilter(excludeFrom: ["SyncEngine.swift"], excludePathWhenMatching: true)]
     return log
 }()

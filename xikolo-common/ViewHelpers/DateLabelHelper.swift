76f2d4660b0a3758820ae0f22d8cc5165dc19b33
Bjarne Sievers <jellyfishcoding@live.de>
2018-01-21 05:33:18 -0600

    Use localized date formatters (#378)

--- xikolo-common/ViewHelpers/DateLabelHelper.swift
+++ xikolo-common/ViewHelpers/DateLabelHelper.swift
@@ -10,6 +10,13 @@ import Foundation
 
 class DateLabelHelper {
 
+    fileprivate static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter.localizedFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        return dateFormatter
+    }()
+
     class func labelFor(startdate: Date?, enddate: Date?) -> String {
         if let start = startdate {
             if let end = enddate {
@@ -45,9 +52,6 @@ class DateLabelHelper {
     }
 
     private class func format(date: Date) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .medium
-        dateFormatter.timeStyle = .none
         return dateFormatter.string(from: date)
     }
 

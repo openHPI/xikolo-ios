51bd5626b2d92048f9f26d0095266557f8397f65
Bjarne Sievers <jellyfishcoding@live.de>
2018-01-21 05:02:52 -0600

    Move UserDefaults synchronize to better place (#495)

--- xikolo-ios/AppDelegate.swift
+++ xikolo-ios/AppDelegate.swift
@@ -68,6 +68,7 @@ class AppDelegate : AbstractAppDelegate {
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
+        UserDefaults.standard.synchronize()
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }

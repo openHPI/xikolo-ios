d13957e63ea70c38ecac433c16c7539546727fae
Max Bothe <max.bothe@hpi.de>
2018-01-22 10:03:45 +0100

    Improve nameing of assets and enum

--- xikolo-ios/Views/PlatformEventCell.swift
+++ xikolo-ios/Views/PlatformEventCell.swift
@@ -16,18 +16,18 @@ class PlatformEventCell: UITableViewCell {
     @IBOutlet weak var dateView: UILabel!
     @IBOutlet weak var timelineView: UIView!
 
-    enum EventTypes : String {
-        case PinboardAnswerComment = "pinboard.question.answer.comment.new"
-        case PinboardQuestionComment = "pinboard.question.comment.new"
-        case PinboardAnswer = "pinboard.question.answer.new"
-        case PinboardQuestion = "pinboard.question.new"
-        case PinboardDiscussionComment = "pinboard.discussion.comment.new"
-        case PinboardDiscussion = "pinboard.discussion.new"
-        case NewsAnnoucement = "news.announcement"
-        case CourseAnnouncement = "course.announcement"
-        case LearningRoomNewFile = "learning_room.new_file"
-        case LearningRoomNewMembership = "learning_room.new_membership"
-        case LearningRoomQuitMembership = "learning_room.quit_membership"
+    enum EventType : String {
+        case pinboardAnswerComment = "pinboard.question.answer.comment.new"
+        case pinboardQuestionComment = "pinboard.question.comment.new"
+        case pinboardAnswer = "pinboard.question.answer.new"
+        case pinboardQuestion = "pinboard.question.new"
+        case pinboardDiscussionComment = "pinboard.discussion.comment.new"
+        case pinboardDiscussion = "pinboard.discussion.new"
+        case newsAnnoucement = "news.announcement"
+        case courseAnnouncement = "course.announcement"
+        case learningRoomNewFile = "learning_room.new_file"
+        case learningRoomNewMembership = "learning_room.new_membership"
+        case learningRoomQuitMembership = "learning_room.quit_membership"
     }
 
     func configure(_ platformEvent: PlatformEvent) {
@@ -44,25 +44,25 @@ class PlatformEventCell: UITableViewCell {
         if let type = platformEvent.type {
             let iconName: String
             switch type {
-            case EventTypes.PinboardAnswerComment.rawValue,
-                 EventTypes.PinboardQuestionComment.rawValue,
-                 EventTypes.PinboardAnswer.rawValue,
-                 EventTypes.PinboardDiscussionComment.rawValue,
-                 EventTypes.PinboardDiscussion.rawValue:
-                iconName = "platform_events-icon-events-comment"
-            case EventTypes.PinboardQuestion.rawValue:
-                iconName = "platform_events-icon-question"
-            case EventTypes.NewsAnnoucement.rawValue,
-                 EventTypes.CourseAnnouncement.rawValue:
-                iconName = "platform_events-icon-mail"
-            case EventTypes.LearningRoomNewFile.rawValue:
-                iconName = "platform_events-icon-file"
-            case EventTypes.LearningRoomNewMembership.rawValue:
-                iconName = "platform_events-icon-user_plus"
-            case EventTypes.LearningRoomQuitMembership.rawValue:
-                iconName = "platform_events-icon-user_times"
+            case EventType.pinboardAnswerComment.rawValue,
+                 EventType.pinboardQuestionComment.rawValue,
+                 EventType.pinboardAnswer.rawValue,
+                 EventType.pinboardDiscussionComment.rawValue,
+                 EventType.pinboardDiscussion.rawValue:
+                iconName = "platform-event-icon-comment"
+            case EventType.pinboardQuestion.rawValue:
+                iconName = "platform-event-icon-question"
+            case EventType.newsAnnoucement.rawValue,
+                 EventType.courseAnnouncement.rawValue:
+                iconName = "platform-event-icon-mail"
+            case EventType.learningRoomNewFile.rawValue:
+                iconName = "platform-event-icon-file"
+            case EventType.learningRoomNewMembership.rawValue:
+                iconName = "platform-event-icon-user-plus"
+            case EventType.learningRoomQuitMembership.rawValue:
+                iconName = "platform-event-icon-user-times"
             default:
-                iconName = "platform_events-icon-mail"
+                iconName = "platform-event-icon-mail"
             }
             categoryView.image = UIImage(named: iconName)
         }

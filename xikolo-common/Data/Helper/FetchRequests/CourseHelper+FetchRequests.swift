1998d7b332abf92a509e4b16557dd810690cf0b8
Max Bothe <max.bothe@hpi.de>
2018-02-16 07:59:25 +0100

    Fix course list in offline mode
    - Show current and upcoming sections
    - Correct fetch request predicates
    - Refactor fetch request predicates

--- xikolo-common/Data/Helper/FetchRequests/CourseHelper+FetchRequests.swift
+++ xikolo-common/Data/Helper/FetchRequests/CourseHelper+FetchRequests.swift
@@ -13,20 +13,31 @@ extension CourseHelper {
     struct FetchRequest {
 
         private static let genericPredicate = NSPredicate(format: "external != %@", NSNumber(booleanLiteral: true))
+
         private static let deletedEnrollmentPrecidate = NSPredicate(format: "enrollment.objectStateValue = %d", ObjectState.deleted.rawValue)
         private static let notDeletedEnrollmentPredicate = NSCompoundPredicate(notPredicateWithSubpredicate: deletedEnrollmentPrecidate)
-        private static let rawEnrolledPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [NSPredicate(format: "enrollment != nil"), notDeletedEnrollmentPredicate])
-        private static let enrolledPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, rawEnrolledPredicate])
-        private static let rawUnenrolledPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [NSPredicate(format: "enrollment = nil"), deletedEnrollmentPrecidate])
-        private static let unenrolledPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, rawUnenrolledPredicate])
+
+        private static let enrolledPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [NSPredicate(format: "enrollment != nil"), notDeletedEnrollmentPredicate])
+        private static let notEnrolledPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [NSPredicate(format: "enrollment = nil"), deletedEnrollmentPrecidate])
+
         private static let announcedPredicate = NSPredicate(format: "status = %@", "announced")
         private static let previewPredicate = NSPredicate(format: "status = %@", "preview")
         private static let activePredicate = NSPredicate(format: "status = %@", "active")
         private static let selfpacedPredicate = NSPredicate(format: "status = %@", "self-paced")
         private static let interestingPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [announcedPredicate, previewPredicate, activePredicate])
         private static let accessiblePredicate = NSPredicate(format: "accessible = %@", NSNumber(booleanLiteral: true))
-        private static let completedPredicate = NSPredicate(format: "enrollment.completed = %@", NSNumber(booleanLiteral: true))
-        private static let notcompletedPredicate = NSCompoundPredicate(notPredicateWithSubpredicate: completedPredicate)
+
+        private static let completedPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
+            enrolledPredicate,
+            NSPredicate(format: "enrollment.completed = %@", NSNumber(booleanLiteral: true)),
+        ])
+        private static let notCompletedPredicate = NSCompoundPredicate(orPredicateWithSubpredicates: [
+            NSCompoundPredicate(andPredicateWithSubpredicates: [
+                enrolledPredicate,
+                NSPredicate(format: "enrollment.completed = %@", NSNumber(booleanLiteral: false))
+            ]),
+            notEnrolledPredicate,
+        ])
 
         static var genericCoursesRequest: NSFetchRequest<Course> {
             let request: NSFetchRequest<Course> = Course.fetchRequest()
@@ -57,81 +68,63 @@ extension CourseHelper {
             return Course.fetchRequest() as NSFetchRequest<Course>
         }
 
-        static var unenrolledCourses: NSFetchRequest<Course> {
-            let request = self.genericCoursesRequest
-            request.predicate = self.unenrolledPredicate
-            return request
-        }
-
-        static var enrolledCourses: NSFetchRequest<Course> {
-            let request = self.genericCoursesRequest
-            request.predicate = enrolledPredicate
-            return request
-        }
-
         static var interestingCoursesRequest: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [unenrolledPredicate, interestingPredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, notEnrolledPredicate, interestingPredicate])
             return request
         }
 
         static var currentCourses: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [accessiblePredicate, notcompletedPredicate, activePredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, accessiblePredicate, notCompletedPredicate, activePredicate])
             return request
         }
 
         static var upcomingCourses: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [announcedPredicate, notcompletedPredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, announcedPredicate, notCompletedPredicate])
             return request
         }
 
         static var selfpacedCourses: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [selfpacedPredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, selfpacedPredicate])
             return request
         }
 
         static var pastCourses: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [unenrolledPredicate, selfpacedPredicate])
-            return request
-        }
-
-        static var enrolledAccessibleCourses: NSFetchRequest<Course> {
-            let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [enrolledPredicate, accessiblePredicate, notcompletedPredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, notEnrolledPredicate, selfpacedPredicate])
             return request
         }
 
         static var enrolledCurrentCoursesRequest: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [enrolledPredicate, accessiblePredicate, notcompletedPredicate, activePredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, enrolledPredicate, accessiblePredicate, notCompletedPredicate, activePredicate])
             return request
         }
 
         static var enrolledSelfPacedCourses: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [enrolledPredicate, accessiblePredicate, notcompletedPredicate, selfpacedPredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, enrolledPredicate, accessiblePredicate, notCompletedPredicate, selfpacedPredicate])
             return request
         }
 
         static var enrolledUpcomingCourses: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [enrolledPredicate, announcedPredicate, notcompletedPredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, enrolledPredicate, announcedPredicate, notCompletedPredicate])
             return request
         }
 
         static var enrolledNotCompletedCourses: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [enrolledPredicate, notcompletedPredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, enrolledPredicate, notCompletedPredicate])
             return request
         }
 
         static var completedCourses: NSFetchRequest<Course> {
             let request = self.genericCoursesRequest
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [enrolledPredicate, completedPredicate])
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [genericPredicate, enrolledPredicate, completedPredicate])
             return request
         }
 

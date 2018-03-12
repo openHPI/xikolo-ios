559e1a4d315e3937dd4d959fac8b257bf9c6b430
Max Bothe <max.bothe@hpi.de>
2018-03-12 14:01:25 +0100

    fix legacy_constructor

--- xikolo-common/Categories/NSMutableAttributedString+trim.swift
+++ xikolo-common/Categories/NSMutableAttributedString+trim.swift
@@ -7,9 +7,8 @@ import Foundation
 
 extension NSMutableAttributedString {
 
-    //  Credit goes to https://stackoverflow.com/a/41300031/7414898
+    // Credit goes to https://stackoverflow.com/a/41300031/7414898
     func trimmedAttributedString(set: CharacterSet) -> NSMutableAttributedString {
-
         let invertedSet = set.inverted
 
         var range = (string as NSString).rangeOfCharacter(from: invertedSet)
@@ -18,7 +17,8 @@ extension NSMutableAttributedString {
         range = (string as NSString).rangeOfCharacter(from: invertedSet, options: .backwards)
         let len = (range.length > 0 ? NSMaxRange(range) : string.count) - loc
 
-        let r = self.attributedSubstring(from: NSMakeRange(loc, len))
+        let r = self.attributedSubstring(from: NSRange(location: loc, length: len))
         return NSMutableAttributedString(attributedString: r)
     }
+
 }

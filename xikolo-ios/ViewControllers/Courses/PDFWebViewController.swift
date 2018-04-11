ae70040e006ef963e90ad1797579b853b862bb89
Bjarne Sievers <jellyfishcoding@live.de>
2018-04-10 23:14:44 -0500

    Adjust pdf controller to consume local data only

--- xikolo-ios/ViewControllers/Courses/PDFWebViewController.swift
+++ xikolo-ios/ViewControllers/Courses/PDFWebViewController.swift
@@ -10,14 +10,13 @@ class PDFWebViewController: UIViewController {
 
     @IBOutlet weak var webView: UIWebView!
 
-    var cachedPdfPath: String?
-    var urlToDownload: String!
+    var cachedPdfPath: URL!
 
     @IBOutlet var shareButton: UIBarButtonItem!
     
     @IBAction func sharePDF(_ sender: UIBarButtonItem) {
-        guard let cachedPdfPath = self.cachedPdfPath, FileManager.default.fileExists(atPath: cachedPdfPath) else { return }
-        let activityItem = NSData(contentsOfFile: cachedPdfPath).require(hint: "Cached PDF isn't a valid file")
+        guard let cachedPdfPath = self.cachedPdfPath, FileManager.default.fileExists(atPath: cachedPdfPath.absoluteString) else { return }
+        let activityItem = NSData(contentsOfFile: cachedPdfPath.absoluteString).require(hint: "Cached PDF isn't a valid file")
         let activityViewController = UIActivityViewController(activityItems: [activityItem], applicationActivities: nil)
         activityViewController.popoverPresentationController?.barButtonItem = sender
         self.present(activityViewController, animated: true)
@@ -25,55 +24,9 @@ class PDFWebViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.rightBarButtonItem = nil
-        self.webView.delegate = self
-        savePdf().onSuccess { (pdfPath) in
-            self.navigationItem.rightBarButtonItem = self.shareButton
-            self.cachedPdfPath = pdfPath
-            self.webView.loadRequest(URLRequest(url: URL(string: pdfPath).require()))
-        }
-    }
-
-    func savePdf() -> Future<String, XikoloError> {
-        let promise = Promise<String, XikoloError>()
-        DispatchQueue.main.async {
-            let fileManager = FileManager.default
-            let path = NSTemporaryDirectory() + "temp.pdf"
-            let pdfDoc = NSData(contentsOf: URL(string: self.urlToDownload)!)
-            if fileManager.createFile(atPath: path, contents: pdfDoc as Data?, attributes: nil) {
-                log.info("Downloaded PDF to temporary location with path: " + path)
-                return promise.success(path)
-            } else {
-                log.error("Couldn't download pdf file with url: \(String(describing: self.urlToDownload))")
-                return promise.failure(XikoloError.totallyUnknownError)
-            }
-        }
-        return promise.future
-    }
-
-    // TODO remove temp file
-
-}
-
-extension PDFWebViewController: UIWebViewDelegate {
-
-    func webViewDidStartLoad(_ webView: UIWebView) {
-        NetworkIndicator.start()
-    }
-
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        NetworkIndicator.end()
-    }
-
-    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        NetworkIndicator.end()
+        let request = URLRequest(url: cachedPdfPath)
+        let data = try! Data.init(contentsOf: cachedPdfPath)
+        webView.load(data, mimeType: "application/pdf", textEncodingName: "UTF-8", baseURL: cachedPdfPath.baseURL!)
     }
 
 }
-
-/*extension PDFWebViewController {
-    override func webViewDidFinishLoad(_ webView: UIWebView) {
-        guard !self.webView.isLoading else { return }
-        currentPdfPath = self.savePdf()
-    }
-}*/

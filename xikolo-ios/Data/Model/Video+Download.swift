033c56e30ae587379291570587615dd3c3f56204
Max Bothe <max.bothe@hpi.de>
2018-06-01 11:06:31 +0200

    Add user actiosn for slides download

--- xikolo-ios/Data/Model/Video+Download.swift
+++ xikolo-ios/Data/Model/Video+Download.swift
@@ -8,18 +8,18 @@ import UIKit
 extension Video {
 
     var userActions: [UIAlertAction] {
-        return [self.videoUserAction].compactMap { $0 }
+        return [self.videoUserAction, self.slidesUserAction].compactMap { $0 }
     }
 
     var videoUserAction: UIAlertAction? {
         let isOffline = ReachabilityHelper.connection == .none
         let downloadState = StreamPersistenceManager.shared.downloadState(for: self)
 
-        if downloadState == .notDownloaded && !isOffline {
+        if let url = self.singleStream?.hlsURL, downloadState == .notDownloaded, !isOffline {
             let downloadActionTitle = NSLocalizedString("course-item.video-download-action.start-download.title",
                                                         comment: "start download of video item")
             return UIAlertAction(title: downloadActionTitle, style: .default) { _ in
-                StreamPersistenceManager.shared.startDownload(for: self)
+                StreamPersistenceManager.shared.startDownload(with:url, for: self)
             }
         }
 
@@ -36,6 +36,34 @@ extension Video {
                                                       comment: "delete video item")
             return UIAlertAction(title: deleteActionTitle, style: .default) { _ in
                 StreamPersistenceManager.shared.deleteDownload(for: self)
+            }
+        }
+
+        return nil
+    }
+
+    var slidesUserAction: UIAlertAction? {
+        let isOffline = ReachabilityHelper.connection == .none
+        let downloadState = SlidesPersistenceManager.shared.downloadState(for: self)
+
+        if let url = self.slidesURL, downloadState == .notDownloaded, !isOffline {
+            let downloadActionTitle = "download slides"
+            return UIAlertAction(title: downloadActionTitle, style: .default) { _ in
+                SlidesPersistenceManager.shared.startDownload(with: url, for: self)
+            }
+        }
+
+        if downloadState == .pending || downloadState == .downloading {
+            let abortActionTitle = "stop slide download"
+            return UIAlertAction(title: abortActionTitle, style: .default) { _ in
+                SlidesPersistenceManager.shared.cancelDownload(for: self)
+            }
+        }
+
+        if downloadState == .downloaded {
+            let deleteActionTitle = "delete slides"
+            return UIAlertAction(title: deleteActionTitle, style: .default) { _ in
+                SlidesPersistenceManager.shared.deleteDownload(for: self)
             }
         }
 

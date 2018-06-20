a8c59eb8e636a8b7fc2a7dbb53879750a50a26bc
Max Bothe <max.bothe@hpi.de>
2018-06-20 10:26:07 +0200

    Move categories

---
+++ xikolo-ios/Extensions/UIView+SafeCastable.swift
@@ -0,0 +1,9 @@
+//
+//  Created for xikolo-ios under MIT license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import UIKit
+
+extension UIView: SafeCastable {}
+extension UIViewController: SafeCastable {}

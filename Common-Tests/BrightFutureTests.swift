95c64f3402be771db4e6e0543d6af1470da6d706
Max Bothe <mathebox@gmail.com>
2018-07-20 15:09:52 +0200

    Add tests for BrightFuture extension (#582)

---
+++ Common-Tests/BrightFutureTests.swift
@@ -0,0 +1,91 @@
+//
+//  Created for xikolo-ios under MIT license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import BrightFutures
+import Result
+import XCTest
+
+@testable import Common
+
+class BrightFutureTests: XCTestCase {
+
+    enum TestError: Error {
+        case someError
+    }
+
+    func testInjectSuccess() {
+        // Given
+        let value = true
+        let outerFuture = Future<Bool, AnyError>(value: value)
+        let innerResult = Result<Void, AnyError>(value: ())
+
+        let outerExpectation = self.expectation(description: "outer")
+        let innerExpectation = self.expectation(description: "inner")
+
+        // When
+        let resultFuture = outerFuture.inject {
+            innerExpectation.fulfill()
+            return innerResult
+        }.onComplete { _ in
+            outerExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+
+        // Then
+        XCTAssertTrue(resultFuture.isSuccess)
+        XCTAssertEqual(resultFuture.value, true)
+    }
+
+    func testInjectOuterFailure() {
+        // Given
+        let error: TestError = .someError
+        let outerFuture = Future<Bool, TestError>(error: error)
+        let innerResult = Result<Void, TestError>(value: ())
+
+        let outerExpectation = self.expectation(description: "outer")
+        let innerExpectation = self.expectation(description: "inner")
+        innerExpectation.isInverted = true
+
+        // When
+        let resultFuture = outerFuture.inject {
+            innerExpectation.fulfill()
+            return innerResult
+        }.onComplete { _ in
+            outerExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+
+        // Then
+        XCTAssertTrue(resultFuture.isFailure)
+        XCTAssertEqual(resultFuture.error, error)
+    }
+
+    func testInjectInnerFailure() {
+        // Given
+        let error: TestError = .someError
+        let outerFuture = Future<Bool, TestError>(value: true)
+        let innerResult = Result<Void, TestError>(error: error)
+
+        let outerExpectation = self.expectation(description: "outer")
+        let innerExpectation = self.expectation(description: "inner")
+
+        // When
+        let resultFuture = outerFuture.inject {
+            innerExpectation.fulfill()
+            return innerResult
+        }.onComplete { _ in
+            outerExpectation.fulfill()
+        }
+
+        waitForExpectations(timeout: 0.3, handler: nil)
+
+        // Then
+        XCTAssertTrue(resultFuture.isFailure)
+        XCTAssertEqual(resultFuture.error, error)
+    }
+
+}

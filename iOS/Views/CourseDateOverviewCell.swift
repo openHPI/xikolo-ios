1c65a85daae1052a5fa418b61bcdd7ec02ecf2a9
Max Bothe <max.bothe@hpi.de>
2018-08-02 08:27:21 +0200

    Add dummy course date overview

---
+++ iOS/Views/CourseDateOverviewCell.swift
@@ -0,0 +1,29 @@
+//
+//  Created for xikolo-ios under MIT license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import Common
+import UIKit
+
+class CourseDateOverviewCell: UITableViewCell {
+
+    @IBOutlet private weak var overviewContainer: UIView!
+    @IBOutlet private weak var nextDateContainer: UIView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.applyCardLook(to: self.overviewContainer)
+        self.applyCardLook(to: self.nextDateContainer)
+    }
+
+    private func applyCardLook(to view: UIView) {
+        view.backgroundColor = Brand.default.colors.secondary.withAlphaComponent(0.2)
+        view.layer.masksToBounds = false
+        view.layer.cornerRadius = 6.0
+        view.layer.shadowOpacity = 0.25
+        view.layer.shadowRadius = 6.0
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOffset = CGSize(width: 0, height: 4)
+    }
+}

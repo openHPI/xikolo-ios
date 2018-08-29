1b296f16c9e15eef0db4736525ab61b91630ba55
Max Bothe <max.bothe@hpi.de>
2018-08-29 16:21:03 +0200

    Trim document description

--- iOS/Views/DocumentHeader.swift
+++ iOS/Views/DocumentHeader.swift
@@ -14,7 +14,7 @@ class DocumentHeader: UITableViewHeaderFooterView {
 
     func configure(for document: Document) {
         self.titleLabel.text = document.title
-        self.descriptionLabel.text = document.documentDescription
+        self.descriptionLabel.text = document.documentDescription?.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
         self.tagsLabel.text = document.tags.isEmpty ? nil : document.tags.joined(separator: " · ")
     }
 

fccae0b17a337ceda4b11a2a5c2d8934b36d0783
Bjarne Sievers <jellyfishcoding@live.de>
2018-12-14 13:44:17 +0100

    WIP Add view

---
+++ iOS/ViewControllers/Courses/LTIHintViewController.swift
@@ -0,0 +1,48 @@
+//
+//  Created for xikolo-ios under MIT license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import Common
+import UIKit
+
+class LTIHintViewController: UIViewController {
+
+    @IBOutlet private weak var itemTitleLabel: UILabel!
+    @IBOutlet private weak var startButton: Button!
+
+    weak var delegate: CourseItemViewController?
+
+    private var courseItem: CourseItem? {
+        didSet {
+            guard self.viewIfLoaded != nil else { return }
+            self.itemTitleLabel.text = self.courseItem?.title
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.itemTitleLabel.text = self.courseItem?.title
+        self.startButton.tintColor = Brand.default.colors.primary
+        self.startButton.titleLabel?.adjustsFontForContentSizeCategory = true
+    }
+
+    @IBAction private func startItem() {
+        guard let item = self.courseItem else { return }
+        self.perf
+
+    }
+
+}
+
+extension LTIHintViewController: CourseItemContentViewController {
+
+    var item: CourseItem? {
+        return self.courseItem
+    }
+
+    func configure(for item: CourseItem) {
+        self.courseItem = item
+    }
+
+}

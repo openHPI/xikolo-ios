9ff2eff191695cb68bc891977993ac3295251b69
Bjarne Sievers <jellyfishcoding@live.de>
2018-12-18 16:42:53 +0100

    WIP version of lit view

--- iOS/ViewControllers/Courses/LTIHintViewController.swift
+++ iOS/ViewControllers/Courses/LTIHintViewController.swift
@@ -9,35 +9,58 @@ import UIKit
 class LTIHintViewController: UIViewController {
 
     @IBOutlet private weak var itemTitleLabel: UILabel!
-    @IBOutlet weak var instructionsView: UILabel!
-    @IBOutlet weak var typeView: UILabel!
-    @IBOutlet weak var pointsView: UILabel!
+    @IBOutlet private weak var instructionsView: UILabel!
+    @IBOutlet private weak var typeView: UILabel!
+    @IBOutlet private weak var pointsView: UILabel!
     @IBOutlet private weak var startButton: UIButton!
 
     weak var delegate: CourseItemViewController?
 
-    private var courseItem: CourseItem? {
+    private var courseItemObserver: ManagedObjectObserver?
+
+    private var courseItem: CourseItem! {
         didSet {
-            guard self.viewIfLoaded != nil else { return }
-            self.itemTitleLabel.text = self.courseItem?.title
+            self.courseItemObserver = ManagedObjectObserver(object: self.courseItem) { [weak self] type in
+                guard type == .update else { return }
+                DispatchQueue.main.async {
+                    self?.updateView()
+                }
+            }
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.updateView()
+
+        CourseItemHelper.syncCourseItemWithContent(self.courseItem)
+    }
+
+    func updateView() {
+        let maxPoints = self.courseItem?.maxPoints
+        let format = NSLocalizedString("course-item.max-points", comment: "maximum points for course item")
+
+        self.pointsView.text = maxPoints.flatMap({ maxPoints -> String? in
+            String.localizedStringWithFormat(format, maxPoints)
+        })
+
         guard let ltiExercise = self.courseItem?.content as? LTIExercise else { return }
         self.itemTitleLabel.text = self.courseItem?.title
         self.startButton.tintColor = Brand.default.colors.primary
         //self.startButton.titleLabel?.adjustsFontForContentSizeCategory = true
         self.instructionsView.text = ltiExercise.instructions
         //self.typeView = ltiExercise.type TODO with new API
-        //self.pointsView.text = self.courseItem.maxPoints
+        //LTIExercise.ExerciseType
+        self.typeView.text = ltiExercise.exerciseType?.rawValue
+        //self.pointsView.text = self.courseItem?.maxPoints
+        //maxPoints.flatMap(String.localizedStringWithFormat(format, $0))
     }
 
     @IBAction private func startItem() {
         guard let item = self.courseItem else { return }
 
-        self.perf
+        //self.perf
 
     }
 

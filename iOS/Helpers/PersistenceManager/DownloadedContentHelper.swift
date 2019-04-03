dc5471917c0597ac1bcc794db497d485e9959b06
Max Bothe <max.bothe@hpi.de>
2019-04-03 12:03:14 +0200

    Split up DownloadedContentListViewController

---
+++ iOS/Helpers/PersistenceManager/DownloadedContentHelper.swift
@@ -0,0 +1,137 @@
+//
+//  Created for xikolo-ios under MIT license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import BrightFutures
+import Common
+import CoreData
+
+enum DownloadedContentHelper {
+
+    struct DownloadItem {
+        var courseID: String
+        var courseTitle: String?
+        var contentType: ContentType
+        var fileSize: UInt64?
+    }
+
+    enum ContentType: CaseIterable {
+        case video
+        case slides
+        case document
+
+        var title: String {
+            switch self {
+            case .video:
+                return NSLocalizedString("settings.downloads.item.video", comment: "download type video")
+            case .slides:
+                return NSLocalizedString("settings.downloads.item.slides", comment: "download type slides")
+            case .document:
+                return NSLocalizedString("settings.downloads.item.document", comment: "download type documents")
+            }
+        }
+
+        var persistenceManager: ContentPersistanceManager {
+            switch self {
+            case .video:
+                return StreamPersistenceManager.shared
+            case .slides:
+                return SlidesPersistenceManager.shared
+            case .document:
+                return DocumentsPersistenceManager.shared
+            }
+        }
+    }
+
+    static func downloadedItemForAllCourses() -> Future<[[DownloadItem]], XikoloError> {
+        var futures = [
+            self.streamCourseIDs(),
+            self.slidesCourseIDs(),
+        ]
+
+        if Brand.default.features.enableDocuments {
+            futures.append(self.documentsCourseIDs())
+        }
+
+        return futures.sequence()
+    }
+
+    private static func streamCourseIDs() -> Future<[DownloadItem], XikoloError> {
+        return self.courseIDs(fetchRequest: VideoHelper.FetchRequest.videosWithDownloadedStream(),
+                              contentType: .video,
+                              keyPath: \Video.item?.section?.course,
+                              persistenceManager: StreamPersistenceManager.shared)
+    }
+
+    private static func slidesCourseIDs() -> Future<[DownloadItem], XikoloError> {
+        return self.courseIDs(fetchRequest: VideoHelper.FetchRequest.videosWithDownloadedSlides(),
+                              contentType: .slides,
+                              keyPath: \Video.item?.section?.course,
+                              persistenceManager: SlidesPersistenceManager.shared)
+    }
+
+    private static func documentsCourseIDs() -> Future<[DownloadItem], XikoloError> {
+        return self.courseIDs(fetchRequest: DocumentLocalizationHelper.FetchRequest.hasDownloadedLocalization(),
+                              contentType: .document,
+                              keyPath: \DocumentLocalization.document.courses,
+                              persistenceManager: DocumentsPersistenceManager.shared)
+    }
+
+    private static func courseIDs<Resource, Manager>(
+        fetchRequest: NSFetchRequest<Resource>,
+        contentType: ContentType,
+        keyPath: KeyPath<Resource, Course?>,
+        persistenceManager: Manager
+    ) -> Future<[DownloadItem], XikoloError> where Manager: PersistenceManager, Manager.Resource == Resource {
+        var items: [DownloadItem] = []
+        let promise = Promise<[DownloadItem], XikoloError>()
+        CoreDataHelper.persistentContainer.performBackgroundTask { privateManagedObjectContext in
+            do {
+                let downloadedItems = try privateManagedObjectContext.fetch(fetchRequest)
+                for video in downloadedItems {
+                    if let course = video[keyPath: keyPath] {
+                        let fileSize = persistenceManager.fileSize(for: video)
+                        items.append(DownloadItem(courseID: course.id, courseTitle: course.title, contentType: contentType, fileSize: fileSize))
+                    }
+                }
+
+                return promise.success(items)
+            } catch {
+                promise.failure(.coreData(error))
+            }
+        }
+
+        return promise.future
+    }
+
+    private static func courseIDs<Resource, Manager>(
+        fetchRequest: NSFetchRequest<Resource>,
+        contentType: ContentType,
+        keyPath: KeyPath<Resource, Set<Course>>,
+        persistenceManager: Manager
+    ) -> Future<[DownloadItem], XikoloError> where Manager: PersistenceManager, Manager.Resource == Resource {
+        var items: [DownloadItem] = []
+        let promise = Promise<[DownloadItem], XikoloError>()
+        CoreDataHelper.persistentContainer.performBackgroundTask { privateManagedObjectContext in
+            do {
+                let downloadedItems = try privateManagedObjectContext.fetch(fetchRequest)
+                for item in downloadedItems {
+                    let downloadItems = item[keyPath: keyPath].map { course -> DownloadItem in
+                        let fileSize = persistenceManager.fileSize(for: item)
+                        return DownloadItem(courseID: course.id, courseTitle: course.title, contentType: contentType, fileSize: fileSize)
+                    }
+
+                    items.append(contentsOf: downloadItems)
+                }
+
+                return promise.success(items)
+            } catch {
+                promise.failure(.coreData(error))
+            }
+        }
+
+        return promise.future
+    }
+
+}
--- iOS/ViewControllers/Account/DownloadedContentListViewController.swift
+++ iOS/ViewControllers/Account/DownloadedContentListViewController.swift
@@ -20,46 +20,11 @@ class DownloadedContentListViewController: UITableViewController {
     struct CourseDownload {
         var id: String
         var title: String
-        var data: [DownloadedContentType: UInt64] = [:]
+        var data: [DownloadedContentHelper.ContentType: UInt64] = [:]
 
         init(id: String, title: String) {
             self.id = id
             self.title = title
-        }
-    }
-
-    private struct DownloadItem {
-        var courseID: String
-        var courseTitle: String?
-        var contentType: DownloadedContentType
-        var fileSize: UInt64?
-    }
-
-    enum DownloadedContentType: CaseIterable {
-        case video
-        case slides
-        case document
-
-        var title: String {
-            switch self {
-            case .video:
-                return NSLocalizedString("settings.downloads.item.video", comment: "download type video")
-            case .slides:
-                return NSLocalizedString("settings.downloads.item.slides", comment: "download type slides")
-            case .document:
-                return NSLocalizedString("settings.downloads.item.document", comment: "download type documents")
-            }
-        }
-
-        var persistenceManager: ContentPersistanceManager {
-            switch self {
-            case .video:
-                return StreamPersistenceManager.shared
-            case .slides:
-                return SlidesPersistenceManager.shared
-            case .document:
-                return DocumentsPersistenceManager.shared
-            }
         }
     }
 
@@ -111,8 +76,8 @@ class DownloadedContentListViewController: UITableViewController {
     }
 
     @discardableResult
-    private func refresh() -> Future<[[DownloadItem]], XikoloError> {
-        return self.courseIDs().onSuccess { itemsArray in
+    private func refresh() -> Future<[[DownloadedContentHelper.DownloadItem]], XikoloError> {
+        return DownloadedContentHelper.downloadedItemForAllCourses().onSuccess { itemsArray in
             let downloadItems = itemsArray.flatMap { $0 }
             var downloadedCourseList: [String: CourseDownload] = [:]
 
@@ -127,96 +92,6 @@ class DownloadedContentListViewController: UITableViewController {
         }.onFailure { error in
             log.error(error.localizedDescription)
         }
-    }
-
-    private func courseIDs() -> Future<[[DownloadItem]], XikoloError> {
-        var futures = [
-            self.streamCourseIDs(),
-            self.slidesCourseIDs(),
-        ]
-
-        if Brand.default.features.enableDocuments {
-            futures.append(self.documentsCourseIDs())
-        }
-
-        return futures.sequence()
-    }
-
-    private func streamCourseIDs() -> Future<[DownloadItem], XikoloError> {
-        return self.courseIDs(fetchRequest: VideoHelper.FetchRequest.videosWithDownloadedStream(),
-                              contentType: .video,
-                              keyPath: \Video.item?.section?.course,
-                              persistenceManager: StreamPersistenceManager.shared)
-    }
-
-    private func slidesCourseIDs() -> Future<[DownloadItem], XikoloError> {
-        return self.courseIDs(fetchRequest: VideoHelper.FetchRequest.videosWithDownloadedSlides(),
-                              contentType: .slides,
-                              keyPath: \Video.item?.section?.course,
-                              persistenceManager: SlidesPersistenceManager.shared)
-    }
-
-    private func documentsCourseIDs() -> Future<[DownloadItem], XikoloError> {
-        return self.courseIDs(fetchRequest: DocumentLocalizationHelper.FetchRequest.hasDownloadedLocalization(),
-                              contentType: .document,
-                              keyPath: \DocumentLocalization.document.courses,
-                              persistenceManager: DocumentsPersistenceManager.shared)
-    }
-
-    private func courseIDs<Resource, Manager>(
-        fetchRequest: NSFetchRequest<Resource>,
-        contentType: DownloadedContentType,
-        keyPath: KeyPath<Resource, Course?>,
-        persistenceManager: Manager
-    ) -> Future<[DownloadItem], XikoloError> where Manager: PersistenceManager, Manager.Resource == Resource {
-        var items: [DownloadItem] = []
-        let promise = Promise<[DownloadItem], XikoloError>()
-        CoreDataHelper.persistentContainer.performBackgroundTask { privateManagedObjectContext in
-            do {
-                let downloadedItems = try privateManagedObjectContext.fetch(fetchRequest)
-                for video in downloadedItems {
-                    if let course = video[keyPath: keyPath] {
-                        let fileSize = persistenceManager.fileSize(for: video)
-                        items.append(DownloadItem(courseID: course.id, courseTitle: course.title, contentType: contentType, fileSize: fileSize))
-                    }
-                }
-
-                return promise.success(items)
-            } catch {
-                promise.failure(.coreData(error))
-            }
-        }
-
-        return promise.future
-    }
-
-    private func courseIDs<Resource, Manager>(
-        fetchRequest: NSFetchRequest<Resource>,
-        contentType: DownloadedContentType,
-        keyPath: KeyPath<Resource, Set<Course>>,
-        persistenceManager: Manager
-    ) -> Future<[DownloadItem], XikoloError> where Manager: PersistenceManager, Manager.Resource == Resource {
-        var items: [DownloadItem] = []
-        let promise = Promise<[DownloadItem], XikoloError>()
-        CoreDataHelper.persistentContainer.performBackgroundTask { privateManagedObjectContext in
-            do {
-                let downloadedItems = try privateManagedObjectContext.fetch(fetchRequest)
-                for item in downloadedItems {
-                    let downloadItems = item[keyPath: keyPath].map { course -> DownloadItem in
-                        let fileSize = persistenceManager.fileSize(for: item)
-                        return DownloadItem(courseID: course.id, courseTitle: course.title, contentType: contentType, fileSize: fileSize)
-                    }
-
-                    items.append(contentsOf: downloadItems)
-                }
-
-                return promise.success(items)
-            } catch {
-                promise.failure(.coreData(error))
-            }
-        }
-
-        return promise.future
     }
 
     private func updateTotalFileSizeLabel() {
@@ -265,7 +140,7 @@ extension DownloadedContentListViewController { // Table view data source
         return cell
     }
 
-    private func downloadType(for indexPath: IndexPath) -> DownloadedContentType {
+    private func downloadType(for indexPath: IndexPath) -> DownloadedContentHelper.ContentType {
         return self.courseDownloads[indexPath.section].data.map { $0.key }[indexPath.row]
     }
 

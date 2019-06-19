6bbba252e001fd49652aff75815eb2e3befdcf00
Max Bothe <max.bothe@hpi.de>
2019-06-19 10:57:43 +0200

    Add support for RTL layouts in all collection view flow layouts

---
+++ iOS/Extensions/UICollectionViewFlowLayout+RTL.swift
@@ -0,0 +1,14 @@
+//
+//  Created for xikolo-ios under MIT license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionViewFlowLayout {
+
+    open override var flipsHorizontallyInOppositeLayoutDirection: Bool {
+        return true
+    }
+
+}

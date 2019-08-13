c81eb682fa76fe9009a7842eee5d285fccc0a18d
Max Bothe <max.bothe@hpi.de>
2019-08-13 10:43:43 +0200

    Remove linter warning

--- iOS/ViewControllers/Courses/CourseViewController.swift
+++ iOS/ViewControllers/Courses/CourseViewController.swift
@@ -62,6 +62,8 @@ class CourseViewController: UIViewController {
         get {
             return self.courseAreaPageViewController?.viewControllers?.first?.toolbarItems
         }
+        // we only want to use the toolbar items of the embedded view controllers
+        // swiftlint:disable:next unused_setter_value
         set {}
     }
 

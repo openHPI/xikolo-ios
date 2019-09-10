c409575131c50d2095f33eb369f564fbbe91ec34
Max Bothe <max.bothe@hpi.de>
2019-09-10 10:06:14 +0200

    Fix navigation bar glitch in video view controller

--- iOS/CourseNavigationController.swift
+++ iOS/CourseNavigationController.swift
@@ -91,22 +91,6 @@ class CourseNavigationController: XikoloNavigationController {
         }
     }
 
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-
-        guard let pageViewController = self.topViewController as? UIPageViewController else {
-            return
-        }
-
-        guard pageViewController.viewControllers?.first is VideoViewController else {
-            return
-        }
-
-        for view in self.view.subviews {
-            view.layer.masksToBounds = !self.navigationBar.isHidden
-        }
-    }
-
     override func dismiss(animated flag: Bool, completion: (() -> Void)? = nil) {
         guard self.presentedViewController != nil else { return }
         super.dismiss(animated: flag, completion: completion)

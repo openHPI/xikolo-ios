8eeae13c562f8cca44340fb143124127d4bad841
Max Bothe <max.bothe@hpi.de>
2019-11-04 10:29:07 +0100

    Improve size of thumb for time slider

--- Binge/BingeTimeSlider.swift
+++ Binge/BingeTimeSlider.swift
@@ -7,13 +7,6 @@ import UIKit
 
 class BingeTimeSlider: UISlider {
 
-    override open func trackRect(forBounds bounds: CGRect) -> CGRect {
-        let trackHeight: CGFloat = 2
-        let superRect = super.trackRect(forBounds: bounds)
-        let heightDelta = trackHeight - superRect.height
-        return CGRect(x: superRect.origin.x, y: superRect.origin.y - heightDelta / 2 - 0.5, width: superRect.width, height: trackHeight)
-    }
-
     override open func thumbRect(forBounds bounds: CGRect, trackRect rect: CGRect, value: Float) -> CGRect {
         var superRect = super.thumbRect(forBounds: bounds, trackRect: rect, value: value)
         guard self.isHighlighted else { return superRect }

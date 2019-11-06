96ace229ae57632a5907ec160a99219586f63347
Max Bothe <max.bothe@hpi.de>
2019-11-06 13:48:15 +0100

    Fix sync

--- Common/Data/Model/CourseProgress.swift
+++ Common/Data/Model/CourseProgress.swift
@@ -10,7 +10,6 @@ import SyncEngine
 public final class CourseProgress: NSManagedObject {
 
     @NSManaged public var id: String
-
     @NSManaged public var mainProgress: ExerciseProgress
     @NSManaged public var selftestProgress: ExerciseProgress
     @NSManaged public var bonusProgress: ExerciseProgress
@@ -19,6 +18,7 @@ public final class CourseProgress: NSManagedObject {
     @nonobjc public class func fetchRequest() -> NSFetchRequest<CourseProgress> {
         return NSFetchRequest<CourseProgress>(entityName: "CourseProgress")
     }
+
 }
 
 extension CourseProgress: JSONAPIPullable {
@@ -28,10 +28,8 @@ extension CourseProgress: JSONAPIPullable {
     }
 
     public func update(from object: ResourceData, with context: SynchronizationContext) throws {
-        let data = try object.value(for: "data") as JSON
-        let attributes = try data.value(for: "attributes") as JSON
-
-        self.mainProgress = try attributes.value(for: "main_exercise")
+        let attributes = try object.value(for: "attributes") as JSON
+        self.mainProgress = try attributes.value(for: "main_exercises")
         self.selftestProgress = try attributes.value(for: "selftest_exercises")
         self.bonusProgress = try attributes.value(for: "bonus_exercises")
         self.visitProgress = try attributes.value(for: "visits")

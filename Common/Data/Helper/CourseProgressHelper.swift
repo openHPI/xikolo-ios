66a7e9e58b3e445c13a0275840eba472771928b5
Max Bothe <max.bothe@hpi.de>
2019-11-06 11:43:38 +0100

    Add course progress helper and fetch requests

---
+++ Common/Data/Helper/CourseProgressHelper.swift
@@ -0,0 +1,20 @@
+//
+//  Created for xikolo-ios under MIT license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import BrightFutures
+import Foundation
+import SyncEngine
+
+
+public enum CourseProgressHelper {
+
+    static func syncPogress(forCourse course: Course) -> Future<SyncSingleResult, XikoloError> {
+        let fetchRequest = Self.FetchRequest.courseProgress(forCourse: course)
+        var query = SingleResourceQuery(type: CourseProgress.self, id: course.id)
+        query.include("section-progresses")
+        return XikoloSyncEngine().synchronize(withFetchRequest: fetchRequest, withQuery: query)
+    }
+
+}

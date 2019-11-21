a364f9639e2decf963c117a121ac382e7c4d33b6
Udo Pigorsch <upigorsch@me.com>
2019-11-21 08:37:29 +0100

    improving section Progress

--- iOS/Views/SectionProgressCell.swift
+++ iOS/Views/SectionProgressCell.swift
@@ -27,49 +27,69 @@ class SectionProgressCell: UITableViewCell {
     @IBOutlet weak var bonusProgressImageView: UIImageView!
     @IBOutlet weak var bonusProgressPointsScored: UILabel!
     @IBOutlet weak var bonusProgressPercentageScored: UILabel!
-    
+
+
     func calculatePercentage(pointsScored: Double, pointsPossible: Double) -> Float {
         if pointsScored == 0 {
-            return Float(0.0)
+            return 0.0
         }
+
+        let percentageFormatter = NumberFormatter()
+        percentageFormatter.numberStyle = NumberFormatter.Style.percent
+        percentageFormatter.maximumFractionDigits = 1
+
         var percentageScored : Double = 0
-        percentageScored = (pointsScored / pointsScored) * 100
+        percentageScored = (pointsScored / pointsPossible)
+
         return Float(percentageScored)
+    }
+
+    func formatToPercentage(number: Float) -> String {
+        let percentageFormatter = NumberFormatter()
+        percentageFormatter.numberStyle = NumberFormatter.Style.percent
+        percentageFormatter.minimumFractionDigits = 1
+        percentageFormatter.maximumFractionDigits = 1
+
+        return percentageFormatter.string(for: number) ?? "0.0"
+
     }
 
     func configureMainProgressStack(for mainProgress: ExerciseProgress) {
         self.mainProgressImageView.image = R.image.courseItemIcons.homework()
 
-        let scoredText = String(format: "%.1f", mainProgress.pointsScored!) + " of " + String(format: "%.1f", mainProgress.pointsPossible!)
-        self.mainProgressPointsScored.text = scoredText
-
         let percentageScored = calculatePercentage(pointsScored: mainProgress.pointsScored!, pointsPossible: mainProgress.pointsPossible!)
-        self.mainProgressPercentageScored.text = String(format: "%.1f", percentageScored) + "%"
-        self.mainProgressView.progress = percentageScored / 100
+
+        self.mainProgressView.progress = percentageScored
+        self.mainProgressPercentageScored.text = formatToPercentage(number: percentageScored)
+
+        let scoredText = String(format: "%.1f", mainProgress.pointsScored!) + " of " + String(format: "%.1f", mainProgress.pointsPossible!) + " scored"
+        self.mainProgressPointsScored.text = scoredText
 
     }
 
     func configureSelfTestProgressStack(for selfTestProgress: ExerciseProgress) {
         self.selfTestProgressImageView.image = R.image.courseItemIcons.quiz()
 
-        let scoredText = String(format: "%.1f", selfTestProgress.pointsScored!) + " of " + String(format: "%.1f", selfTestProgress.pointsPossible!)
+        let scoredText = String(format: "%.1f", selfTestProgress.pointsScored!) + " of " + String(format: "%.1f", selfTestProgress.pointsPossible!) + " scored"
         self.selfTestProgressPointsScored.text = scoredText
 
         let percentageScored = calculatePercentage(pointsScored: selfTestProgress.pointsScored!, pointsPossible: selfTestProgress.pointsPossible!)
-        self.selfTestProgressPercentageScored.text = String(format: "%.1f", percentageScored) + "%"
-        self.selfTestProgressView.progress = percentageScored / 100
+
+        self.selfTestProgressView.progress = percentageScored
+        self.selfTestProgressPercentageScored.text = formatToPercentage(number: percentageScored)
 
     }
 
     func configureBonusProgressStack(for bonusProgress: ExerciseProgress) {
         self.selfTestProgressImageView.image = R.image.courseItemIcons.bonusQuiz()
 
-        let scoredText = String(format: "%.1f", bonusProgress.pointsScored!) + " of " + String(format: "%.1f", bonusProgress.pointsPossible!)
+        let scoredText = String(format: "%.1f", bonusProgress.pointsScored!) + " of " + String(format: "%.1f", bonusProgress.pointsPossible!) + " scored"
         self.bonusProgressPointsScored.text = scoredText
 
         let percentageScored = calculatePercentage(pointsScored: bonusProgress.pointsScored!, pointsPossible: bonusProgress.pointsPossible!)
-        self.bonusProgressPercentageScored.text = String(format: "%.1f", percentageScored) + "%"
-        self.bonusProgressView.progress = percentageScored / 100
+
+        self.bonusProgressView.progress = percentageScored
+        self.bonusProgressPercentageScored.text = formatToPercentage(number: percentageScored)
 
     }
 
@@ -77,10 +97,23 @@ class SectionProgressCell: UITableViewCell {
 
         self.sectionProgressTitle.text = sectionProgress.title
 
-        configureMainProgressStack(for: sectionProgress.mainProgress)
-        configureSelfTestProgressStack(for: sectionProgress.selftestProgress)
-        configureBonusProgressStack(for: sectionProgress.bonusProgress)
+        if sectionProgress.mainProgress.pointsPossible! > 0.0 {
+            configureMainProgressStack(for: sectionProgress.mainProgress)
+        } else {
+            self.mainProgressStackView.isHidden = true
+        }
 
+        if sectionProgress.selftestProgress.pointsPossible! > 0.0 {
+            configureSelfTestProgressStack(for: sectionProgress.selftestProgress)
+        } else {
+            self.selfTestProgressStackView.isHidden = true
+        }
+
+        if sectionProgress.bonusProgress.pointsPossible! > 0.0 {
+            configureBonusProgressStack(for: sectionProgress.bonusProgress)
+        } else {
+            self.bonusProgressStackView.isHidden = true
+        }
     }
 
 }

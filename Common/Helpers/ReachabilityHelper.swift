fec0bddbb9bfeb123bfe07527bbe3fccd6d24ca0
Max Bothe <max.bothe@hpi.de>
2019-11-29 11:43:53 +0100

    Fix linter warning

--- Common/Helpers/ReachabilityHelper.swift
+++ Common/Helpers/ReachabilityHelper.swift
@@ -9,7 +9,7 @@ import Reachability
 public enum ReachabilityHelper {
 
     static var reachability: Reachability = {
-        return try! Reachability(hostname: Brand.default.host)
+        return try! Reachability(hostname: Brand.default.host) // swiftlint:disable:this force_try
     }()
 
     public static var connection: Reachability.Connection {

f1e25b901b10a4e1e4518a1372748708b0829020
caterinamandel98 <catze98@gmx.de>
2020-01-23 10:16:18 +0100

    implementation of the UIActivityItemSource protocol for courseItem

--- iOS/ViewControllers/CoreDataTableViewDataSource.swift
+++ iOS/ViewControllers/CoreDataTableViewDataSource.swift
@@ -154,6 +154,7 @@ class CoreDataTableViewDataSource<Delegate: CoreDataTableViewDataSourceDelegate>
                 self.tableView?.deleteRows(at: [indexPath], with: .fade)
                 self.tableView?.insertRows(at: [newIndexPath], with: .fade)
             }
+
         @unknown default:
             break
         }

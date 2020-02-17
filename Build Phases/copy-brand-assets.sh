f205e45e1f0729995adf688f3c291cdd3f11f334
Max Bothe <max.bothe@hpi.de>
2020-02-17 11:37:00 +0100

    Distinugsh between brand name and product name

--- Build Phases/copy-brand-assets.sh
+++ Build Phases/copy-brand-assets.sh
@@ -2,4 +2,4 @@
 
 set -x
 rm -rf ${PROJECT_DIR}/iOS/assets-ios-brand.generated.xcassets
-cp -R ${PROJECT_DIR}/iOS/Branding/${PRODUCT_NAME}/assets-ios-brand.xcassets ${PROJECT_DIR}/iOS/assets-ios-brand.generated.xcassets
+cp -R ${PROJECT_DIR}/iOS/Branding/${BRAND_NAME}/assets-ios-brand.xcassets ${PROJECT_DIR}/iOS/assets-ios-brand.generated.xcassets

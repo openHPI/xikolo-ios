751ddad07c6e673072e9020177beae3d5a6374f3
Max Bothe <max.bothe@hpi.de>
2020-02-17 12:01:56 +0100

    Remove unused keychain group identifier

--- Common/Extensions/Bundle.swift
+++ Common/Extensions/Bundle.swift
@@ -6,11 +6,9 @@
 import Foundation
 
 public extension Bundle {
+
     var appGroupIdentifier: String? {
         return self.infoDictionary?["APP_GROUP_IDENTIFIER"] as? String
     }
 
-    var keychainGroupIdentifier: String? {
-        return self.infoDictionary?["KEYCHAIN_GROUP"] as? String
-    }
 }

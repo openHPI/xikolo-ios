4d9d8483e2fbbe8fd15b412e778649049c0c3ad3
Max Bothe <max.bothe@hpi.de>
2020-02-28 14:41:54 +0100

    Respect deadlines of LTI exercises

--- iOS/ViewControllers/Courses/LTIExerciseHintViewController.swift
+++ iOS/ViewControllers/Courses/LTIExerciseHintViewController.swift
@@ -6,7 +6,7 @@
 import Common
 import UIKit
 
-class LTIHintViewController: UIViewController {
+class LTIExerciseHintViewController: UIViewController {
 
     private static let pointsFormatter: NumberFormatter = {
         let formatter = NumberFormatter()
@@ -16,11 +16,17 @@ class LTIHintViewController: UIViewController {
         return formatter
     }()
 
+    private static let dateFormatter = DateFormatter.localizedFormatter(dateStyle: .long, timeStyle: .long)
+
     @IBOutlet private weak var itemTitleLabel: UILabel!
+    @IBOutlet private weak var exerciseTypeLabel: UILabel!
+    @IBOutlet private weak var pointsLabel: UILabel!
+    @IBOutlet private weak var deadlineDateView: UIStackView!
+    @IBOutlet private weak var deadlineLabel: UILabel!
+    @IBOutlet private weak var launchInfoView: UIStackView!
     @IBOutlet private weak var instructionsView: UITextView!
-    @IBOutlet private weak var typeView: UILabel!
-    @IBOutlet private weak var pointsView: UILabel!
-    @IBOutlet private weak var startButton: UIButton!
+    @IBOutlet private weak var launchButton: UIButton!
+    @IBOutlet private weak var deadlineExpiredView: UIStackView!
 
     weak var delegate: CourseItemViewController?
 
@@ -40,7 +46,8 @@ class LTIHintViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.startButton.layer.roundCorners(for: .default)
+        self.launchButton.layer.roundCorners(for: .default)
+        self.launchButton.backgroundColor = Brand.default.colors.primary
 
         self.instructionsView.delegate = self
         self.instructionsView.textContainerInset = UIEdgeInsets.zero
@@ -53,41 +60,51 @@ class LTIHintViewController: UIViewController {
     func updateView() {
         guard let ltiExercise = self.courseItem?.content as? LTIExercise else { return }
 
-        self.itemTitleLabel.text = self.courseItem?.title
-        self.startButton.backgroundColor = Brand.default.colors.primary
+        let deadlineExpired = self.courseItem?.deadline?.inPast ?? false
+        self.deadlineExpiredView.isHidden = !deadlineExpired
+        self.launchInfoView.isHidden = deadlineExpired
 
+        self.itemTitleLabel.text = self.courseItem?.title
+
+        // Set exercise type label
+        switch self.courseItem.exerciseType {
+        case "main":
+            self.exerciseTypeLabel.text = NSLocalizedString("course.item.exercise-type.disclaimer.main", comment: "course item main type")
+        case "bonus":
+            self.exerciseTypeLabel.text = NSLocalizedString("course.item.exercise-type.disclaimer.bonus", comment: "course item bonus type")
+        case "selftest":
+            self.exerciseTypeLabel.text = NSLocalizedString("course.item.exercise-type.disclaimer.ungraded", comment: "course item ungraded type")
+        default:
+            self.exerciseTypeLabel.text = nil
+        }
+
+        // Set points label
+        let format = NSLocalizedString("course-item.max-points", comment: "maximum points for course item")
+        let number = NSNumber(value: self.courseItem.maxPoints)
+        self.pointsLabel.text = Self.pointsFormatter.string(from: number).flatMap { String.localizedStringWithFormat(format, $0) }
+
+        // Set deadline label
+        self.deadlineLabel.text = self.courseItem.deadline.map(Self.dateFormatter.string(from:))
+        self.deadlineDateView.isHidden = self.courseItem.deadline == nil
+
+        // Set instructions label
         if let markdown = ltiExercise.instructions {
             MarkdownHelper.attributedString(for: markdown).onSuccess { [weak self] attributedString in
                 self?.instructionsView.attributedText = attributedString
                 self?.instructionsView.isHidden = false
             }
         }
-
-        switch self.courseItem.exerciseType {
-        case "main":
-            self.typeView.text = NSLocalizedString("course.item.exercise-type.disclaimer.main", comment: "course item main type")
-        case "bonus":
-            self.typeView.text = NSLocalizedString("course.item.exercise-type.disclaimer.bonus", comment: "course item bonus type")
-        case "selftest":
-            self.typeView.text = NSLocalizedString("course.item.exercise-type.disclaimer.ungraded", comment: "course item ungraded type")
-        default:
-            self.typeView.isHidden = true
-        }
-
-        let format = NSLocalizedString("course-item.max-points", comment: "maximum points for course item")
-        let number = NSNumber(value: self.courseItem.maxPoints)
-        self.pointsView.text = Self.pointsFormatter.string(from: number).flatMap { String.localizedStringWithFormat(format, $0) }
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let item = self.courseItem, let ltiExercise = item.content as? LTIExercise else { return }
-        if let typedInfo = R.segue.ltiHintViewController.openLTIURL(segue: segue) {
+        if let typedInfo = R.segue.ltiExerciseHintViewController.openLTIURL(segue: segue) {
             typedInfo.destination.url = ltiExercise.launchURL
         }
     }
 }
 
-extension LTIHintViewController: UITextViewDelegate {
+extension LTIExerciseHintViewController: UITextViewDelegate {
 
     func textView(_ textView: UITextView, shouldInteractWith URL: URL, in characterRange: NSRange, interaction: UITextItemInteraction) -> Bool {
         guard let appNavigator = self.appNavigator else { return false }
@@ -96,7 +113,7 @@ extension LTIHintViewController: UITextViewDelegate {
 
 }
 
-extension LTIHintViewController: CourseItemContentPresenter {
+extension LTIExerciseHintViewController: CourseItemContentPresenter {
 
     var item: CourseItem? {
         return self.courseItem

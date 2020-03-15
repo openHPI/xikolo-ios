f9a56f22533590b949531e6141d9be47e0263fa1
Max Bothe <max.bothe@hpi.de>
2020-03-15 17:04:23 +0100

    Remove today extension

--- Build Phases/remove-localizations.sh
+++ Build Phases/remove-localizations.sh
@@ -3,5 +3,4 @@
 set -x
 if [ "${BRAND_NAME}" == "openSAP" ]; then
     rm -r "${TARGET_BUILD_DIR}/${PRODUCT_NAME}.app/de.lproj"
-    rm -r "${TARGET_BUILD_DIR}/${PRODUCT_NAME}.app/Plugins/today.appex/de.lproj"
 fi

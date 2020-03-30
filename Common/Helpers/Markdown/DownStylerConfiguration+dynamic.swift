81c1f10dcbe5918fd43a2dc0a02f262da50a42e3
Max Bothe <max.bothe@hpi.de>
2020-03-30 14:45:44 +0200

    Fix initial font size for markdown texts

---
+++ Common/Helpers/Markdown/DownStylerConfiguration+dynamic.swift
@@ -0,0 +1,68 @@
+//
+//  Created for xikolo-ios under MIT license.
+//  Copyright © HPI. All rights reserved.
+//
+
+import Down
+
+extension DownStylerConfiguration {
+
+    static func makeDynamicConfiguration() -> DownStylerConfiguration {
+        return DownStylerConfiguration(
+            fonts: self.dynamicFontCollection,
+            colors: self.dynamicColors
+        )
+    }
+
+    private static var dynamicFontCollection: StaticFontCollection {
+        return StaticFontCollection(
+            heading1: Self.makeDynamicFont(for: .systemFont(ofSize: UIFont.labelFontSize * 1.30, weight: .bold)),
+            heading2: Self.makeDynamicFont(for: .systemFont(ofSize: UIFont.labelFontSize * 1.25, weight: .bold)),
+            heading3: Self.makeDynamicFont(for: .systemFont(ofSize: UIFont.labelFontSize * 1.20, weight: .bold)),
+            body: .preferredFont(forTextStyle: .body),
+            code: dynamicCodeFont,
+            listItemPrefix: Self.makeDynamicFont(for: DownFont.monospacedDigitSystemFont(ofSize: UIFont.labelFontSize * 1.0, weight: .regular))
+        )
+    }
+
+    private static var dynamicCodeFont: DownFont {
+        if #available(iOS 13, *) {
+            return .monospacedSystemFont(ofSize: UIFont.labelFontSize, weight: .regular)
+        } else if let menlo = UIFont(name: "menlo", size: UIFont.labelFontSize) {
+            return menlo
+        } else if let courierNew = UIFont(name: "Courier New", size: UIFont.labelFontSize) {
+            return courierNew
+        } else {
+            return .systemFont(ofSize: UIFont.labelFontSize)
+        }
+    }
+
+    private static func makeDynamicFont(for font: UIFont) -> UIFont {
+        if #available(iOS 11, *) {
+            return UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        } else {
+            return font
+        }
+    }
+
+    private static var dynamicColors: ColorCollection {
+        if #available(iOS 13, *) {
+            return StaticColorCollection(
+                heading1: .label,
+                heading2: .label,
+                heading3: .label,
+                body: .label,
+                code: .label,
+                link: Brand.default.colors.window,
+                quote: .secondaryLabel,
+                quoteStripe: .secondaryLabel,
+                thematicBreak: .separator,
+                listItemPrefix: .tertiaryLabel,
+                codeBlockBackground: .secondarySystemBackground
+            )
+        } else {
+            return StaticColorCollection()
+        }
+    }
+
+}
--- Common/Helpers/Markdown/MarkdownHelper.swift
+++ Common/Helpers/Markdown/MarkdownHelper.swift
@@ -7,60 +7,6 @@ import Down
 
 public enum MarkdownHelper {
 
-    static func makeDynamicFont(for font: UIFont) -> UIFont {
-        if #available(iOS 11, *) {
-            return UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
-        } else {
-            return font
-        }
-    }
-
-    static let dynamicCodeFont: DownFont = {
-        if #available(iOS 13, *) {
-            return .monospacedSystemFont(ofSize: UIFont.labelFontSize, weight: .regular)
-        } else if let menlo = UIFont(name: "menlo", size: UIFont.labelFontSize) {
-            return menlo
-        } else if let courierNew = UIFont(name: "Courier New", size: UIFont.labelFontSize) {
-            return courierNew
-        } else {
-            return .systemFont(ofSize: UIFont.labelFontSize)
-        }
-    }()
-
-    static let dynamicFontCollection = StaticFontCollection(
-        heading1: Self.makeDynamicFont(for: .systemFont(ofSize: UIFont.labelFontSize * 1.30, weight: .bold)),
-        heading2: Self.makeDynamicFont(for: .systemFont(ofSize: UIFont.labelFontSize * 1.25, weight: .bold)),
-        heading3: Self.makeDynamicFont(for: .systemFont(ofSize: UIFont.labelFontSize * 1.20, weight: .bold)),
-        body: .preferredFont(forTextStyle: .body),
-        code: dynamicCodeFont,
-        listItemPrefix: Self.makeDynamicFont(for: DownFont.monospacedDigitSystemFont(ofSize: UIFont.labelFontSize * 1.0, weight: .regular))
-    )
-
-    static let dynamicColors: ColorCollection = {
-        if #available(iOS 13, *) {
-            return StaticColorCollection(
-                heading1: .label,
-                heading2: .label,
-                heading3: .label,
-                body: .label,
-                code: .label,
-                link: Brand.default.colors.window,
-                quote: .secondaryLabel,
-                quoteStripe: .secondaryLabel,
-                thematicBreak: .separator,
-                listItemPrefix: .tertiaryLabel,
-                codeBlockBackground: .secondarySystemBackground
-            )
-        } else {
-            return StaticColorCollection()
-        }
-    }()
-
-    static let dynamicTypeStylerConfiguration = DownStylerConfiguration(
-        fonts: dynamicFontCollection,
-        colors: dynamicColors
-    )
-
     static func dataTask(for url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionTask {
         guard let absoluteURL = URL(string: url.absoluteString, relativeTo: Routes.base) else {
             return URLSession.shared.dataTask(with: url, completionHandler: completionHandler)
@@ -79,7 +25,7 @@ public enum MarkdownHelper {
 
     public static func attributedString(for markdown: String) -> NSAttributedString {
         let down = Down(markdownString: markdown)
-        let styler = NoImagesStyler(configuration: self.dynamicTypeStylerConfiguration)
+        let styler = NoImagesStyler(configuration: DownStylerConfiguration.makeDynamicConfiguration())
         let attributedString = try? down.toAttributedString(.smartUnsafe, styler: styler)
         return attributedString ?? NSAttributedString()
     }
@@ -88,7 +34,7 @@ public enum MarkdownHelper {
         let down = Down(markdownString: markdown)
         let styler = AsyncImagesStyler(imageLoader: self.dataTask(for:completionHandler:),
                                        layoutChangeHandler: layoutChangeHandler,
-                                       configuration: self.dynamicTypeStylerConfiguration)
+                                       configuration: DownStylerConfiguration.makeDynamicConfiguration())
         let attribtuedString = try? down.toAttributedString(.smartUnsafe, styler: styler)
         return attribtuedString ?? NSAttributedString()
     }

cac20b5660e31f38910d36c358265f391c1d4a2a
Udo Pigorsch <upigorsch@me.com>
2020-04-24 11:25:18 +0200

    Get rid of hard coded branding

--- Common/Extensions/Bundle.swift
+++ Common/Extensions/Bundle.swift
@@ -15,4 +15,7 @@ public extension Bundle {
         return self.infoDictionary?["URL_SCHEME"] as? String
     }
 
+    var courseOpen: String? {
+        return self.infoDictionary?["COURSE_OPEN"] as? String
+    }
 }

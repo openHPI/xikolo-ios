90dd46ab607cb963266419320fe04c8ace40971b
Udo Pigorsch <6067406+cH3n7i@users.noreply.github.com>
2020-06-15 07:50:54 +0200

    Support Drag & Drop Course into other Apps (#790)
    * Add NSItemProviderWriting  protocol to course model
    * Add URL drop representation
    
    Co-authored-by: Max Bothe <max.bothe@hpi.de>

--- iOS/Extensions/Course+DragItem.swift
+++ iOS/Extensions/Course+DragItem.swift
@@ -4,13 +4,14 @@
 //
 
 import Common
+import MobileCoreServices // for kUTTypeURL
 
 @available(iOS 11.0, *)
 extension Course {
 
     func dragItem(with previewView: UIView?) -> UIDragItem {
         let userActivity = self.openCourseUserActivity
-        let itemProvider = NSItemProvider()
+        let itemProvider = NSItemProvider(object: self)
         itemProvider.registerObject(userActivity, visibility: .all)
         let dragItem = UIDragItem(itemProvider: itemProvider)
         dragItem.localObject = self
@@ -25,4 +26,49 @@ extension Course {
         return dragItem
     }
 
+}
+
+extension Course: NSItemProviderWriting {
+    // MARK: - NSItemProviderWriting
+
+    public static var writableTypeIdentifiersForItemProvider: [String] {
+        return [
+            kUTTypeUTF8PlainText as String,
+            kUTTypeURL as String,
+        ]
+    }
+
+    public func loadData(withTypeIdentifier typeIdentifier: String,
+                         forItemProviderCompletionHandler completionHandler: @escaping (Data?, Error?) -> Void) -> Progress? {
+        if typeIdentifier == kUTTypeUTF8PlainText as String {
+            let titleUrl = [self.title, self.url?.absoluteString].compactMap({ $0 }).joined(separator: "\n")
+            completionHandler(titleUrl.data(using: .utf8), nil)
+        } else if typeIdentifier == kUTTypeURL as String {
+            let dropRepresentation = self.url.flatMap({ URLDropRepresentation(url: $0, title: self.title) })
+            let data = try? dropRepresentation.map(PropertyListEncoder().encode)
+            completionHandler(data, nil)
+        }
+
+        return nil
+    }
+}
+
+private struct URLDropRepresentation: Encodable {
+    let url: URL
+    let title: String?
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        try container.encode(self.url.absoluteString)
+        try container.encode("")
+        try container.encode(self.metadata)
+    }
+
+    private var metadata: [String: String] {
+        guard let title = self.title else {
+            return [:]
+        }
+
+        return ["title": title]
+    }
 }

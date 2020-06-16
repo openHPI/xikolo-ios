40152a356e91aa4e4a13306beb1f2ad1c40a3799
Max Bothe <max.bothe@hpi.de>
2020-06-16 14:36:33 +0200

    Move code

--- Common/Data/Model/CourseItem.swift
+++ Common/Data/Model/CourseItem.swift
@@ -98,13 +98,3 @@ extension CourseItem: JSONAPIPushable {
     }
 
 }
-
-extension CourseItem {
-
-    public var url: URL? {
-        guard let courseSlug = self.section?.course?.slug else { return nil }
-        guard let courseItemId = self.base62id else { return nil }
-        return Routes.courses.appendingPathComponents([courseSlug, "items", courseItemId])
-    }
-
-}

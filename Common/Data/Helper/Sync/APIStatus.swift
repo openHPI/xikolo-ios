8602265bf4d9a7146ed986b2c629390ab7ce5485
Max Bothe <max.bothe@hpi.de>
2020-11-17 16:20:04 +0100

    Remove synthesizable implementation for Equatable conformance of APIStatus

--- Common/Data/Helper/Sync/APIStatus.swift
+++ Common/Data/Helper/Sync/APIStatus.swift
@@ -6,27 +6,10 @@
 import Foundation
 
 public enum APIStatus: Equatable {
-
     case standard
     case maintenance
     case deprecated(expiresOn: Date)
     case expired
-
-    public static func == (lhs: APIStatus, rhs: APIStatus) -> Bool {
-        switch (lhs, rhs) {
-        case (.standard, .standard):
-            return true
-        case (.maintenance, .maintenance):
-            return true
-        case let (.deprecated(lhsDate), .deprecated(rhsDate)):
-            return lhsDate == rhsDate
-        case (.expired, .expired):
-            return true
-        default:
-            return false
-        }
-    }
-
 }
 
 extension APIStatus {

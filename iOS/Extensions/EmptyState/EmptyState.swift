5bcee28730f291f095dcdf56db2d1b720ccdea1e
Max Bothe <max.bothe@hpi.de>
2021-10-26 14:06:47 +0200

    Avoid reference cycle caused by empty state configuration

--- iOS/Extensions/EmptyState/EmptyState.swift
+++ iOS/Extensions/EmptyState/EmptyState.swift
@@ -66,7 +66,7 @@ extension EmptyStateProtocol {
         }
         set {
             if let newValue = newValue {
-                objc_setAssociatedObject(self, &AssociatedKeys.emptyStateDelegate, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                objc_setAssociatedObject(self, &AssociatedKeys.emptyStateDelegate, newValue, .OBJC_ASSOCIATION_ASSIGN)
             }
         }
     }
@@ -78,7 +78,7 @@ extension EmptyStateProtocol {
         }
         set {
             if let newValue = newValue {
-                objc_setAssociatedObject(self, &AssociatedKeys.emptyStateDataSource, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+                objc_setAssociatedObject(self, &AssociatedKeys.emptyStateDataSource, newValue, .OBJC_ASSOCIATION_ASSIGN)
                 self.reloadEmptyState()
             }
         }

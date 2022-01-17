2c08a881e516265bf55a5bb7ead40ffb33053454
Max Bothe <max.bothe@hpi.de>
2022-01-17 19:46:01 +0100

    Add non-breaking spaces to formatted course periods

--- Common/Helpers/CoursePeriodFormatter.swift
+++ Common/Helpers/CoursePeriodFormatter.swift
@@ -17,14 +17,18 @@ public enum CoursePeriodFormatter {
     static func string(fromStartDate startDate: Date?, endDate: Date?, withStyle style: CourseDateLabelStyle = Brand.default.courseDateLabelStyle) -> String {
         if let endDate = endDate, endDate.inPast {
             let format = CommonLocalizedString("course-date-formatting.self-paced since %@", comment: "Self-paced course (since end date)")
-            return String.localizedStringWithFormat(format, self.dateFormatter.string(from: endDate))
+            let formattedDate = self.dateFormatter.string(from: endDate)
+            let nonBreakingFormattedDate = formattedDate.replacingOccurrences(of: " ", with: "\u{00a0}")
+            return String.localizedStringWithFormat(format, nonBreakingFormattedDate)
         }
 
         if let startDate = startDate, startDate.inPast, endDate == nil {
             switch style {
             case .normal:
                 let format = CommonLocalizedString("course-date-formatting.started.since %@", comment: "course start at specific date in the past")
-                return String.localizedStringWithFormat(format, self.dateFormatter.string(from: startDate))
+                let formattedDate = self.dateFormatter.string(from: startDate)
+                let nonBreakingFormattedDate = formattedDate.replacingOccurrences(of: " ", with: "\u{00a0}")
+                return String.localizedStringWithFormat(format, nonBreakingFormattedDate)
             case .who:
                 return CommonLocalizedString("course-date-formatting.self-paced", comment: "Self-paced course")
             }
@@ -34,7 +38,9 @@ public enum CoursePeriodFormatter {
             switch style {
             case .normal:
                 let format = CommonLocalizedString("course-date-formatting.not-started.beginning %@", comment: "course start at specific date in the future")
-                return String.localizedStringWithFormat(format, self.dateFormatter.string(from: startDate))
+                let formattedDate = self.dateFormatter.string(from: startDate)
+                let nonBreakingFormattedDate = formattedDate.replacingOccurrences(of: " ", with: "\u{00a0}")
+                return String.localizedStringWithFormat(format, nonBreakingFormattedDate)
             case .who:
                 return CommonLocalizedString("course-date-formatting.not-started.coming soon", comment: "course start at unknown date")
             }
